fd00e1db83c71a384c1f97bb19c7b50068633094
Helen Chapman <helen.chapman@torchbox.com>
2013-08-23 13:32:50 +0100

    importing Dave's menu css and js, working on styling of menu and search (bit of a halfway commit)

--- rca/site/assets/js/site.js
+++ rca/site/assets/js/site.js
@@ -2,18 +2,17 @@
 /* generic function to show / hide elements
  * the argument element will be assigned or unassigned an 'expanded' class.
  * The rest should be handled by the css */
-function expand(element) {
-
-}
-
-
-$(document).ready(function(){
-	$('li.main').click(function(eventObject){
+function showHide(element) {
+	$(element).click(function(eventObject){
 		if($(this).hasClass('expanded')) {
 			$(this).removeClass('expanded');
 		} else {
 			$(this).addClass('expanded');
 		}
 	});
-});
+}
 
+
+$(document).ready(function(){
+	showHide('li.main'); /* footer expand / collapse */
+});
