32cfd11cb834f58a6b5d85394bb186c29a18c7f9
Matt Westcott <matt@west.co.tt>
2013-08-30 18:34:34 +0100

    convert indentation to spaces. The person responsible has been shot, etc

--- django-verdant/core/models.py
+++ django-verdant/core/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 
 class SiteManager(models.Manager):
-	def get_by_natural_key(self, hostname):
-		return self.get(hostname=hostname)
+    def get_by_natural_key(self, hostname):
+        return self.get(hostname=hostname)
 
 
 class Site(models.Model):
-	hostname = models.CharField(max_length=255, unique=True)
-	root_page = models.ForeignKey('Page')
+    hostname = models.CharField(max_length=255, unique=True)
+    root_page = models.ForeignKey('Page')
 
-	def natural_key(self):
-		return (self.name,)
+    def natural_key(self):
+        return (self.name,)
 
 
 class Page(models.Model):
-	title = models.CharField(max_length=255)
-	slug = models.SlugField()
-	parent = models.ForeignKey('self', blank=True, null=True, related_name='subpages')
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='subpages')
