e40e54fc6f52140e46f41b9e1f45d0307b545118
Matt Westcott <matt@west.co.tt>
2013-08-30 18:32:36 +0100

    Add initial data for site/page

--- django-verdant/core/models.py
+++ django-verdant/core/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
+
+class SiteManager(models.Manager):
+	def get_by_natural_key(self, hostname):
+		return self.get(hostname=hostname)
+
+
 class Site(models.Model):
-	hostname = models.CharField(max_length=255)
+	hostname = models.CharField(max_length=255, unique=True)
 	root_page = models.ForeignKey('Page')
+
+	def natural_key(self):
+		return (self.name,)
+
 
 class Page(models.Model):
 	title = models.CharField(max_length=255)
