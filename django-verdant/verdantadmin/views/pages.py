4bd84d9bb183bb7c377aef24730eca83ee6c6494
Matt Westcott <matt@west.co.tt>
2013-09-02 13:11:35 +0100

    add view for creating new pages

--- django-verdant/verdantadmin/views/pages.py
+++ django-verdant/verdantadmin/views/pages.py
@@ -1,6 +1,8 @@
+from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
 
 from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
 
 from core.models import Page, get_page_types
 
@@ -11,11 +13,42 @@ def index(request):
         'pages': pages,
     })
 
+
 def select_type(request):
     page_types = get_page_types()
     return render(request, 'verdantadmin/pages/select_type.html', {
         'page_types': page_types,
     })
+
+
+def create(request, content_type_id):
+    try:
+        content_type = ContentType.objects.get_for_id(content_type_id)
+    except ContentType.DoesNotExist:
+        raise Http404
+
+    # content type must be in the list of page types
+    if content_type not in get_page_types():
+        raise Http404
+
+    page_class = content_type.model_class()
+    form_class = page_class.form_class
+
+    if request.POST:
+        form = form_class(request.POST)
+        if form.is_valid():
+            page = form.save()
+            messages.success(request, "Page '%s' created." % page.title)
+            return redirect('verdantadmin_pages_index')
+    else:
+        form = form_class()
+
+    return render(request, 'verdantadmin/pages/create.html', {
+        'content_type': content_type,
+        'page_class': page_class,
+        'form': form,
+    })
+
 
 def edit(request, page_id):
     page = get_object_or_404(Page, id=page_id).specific
