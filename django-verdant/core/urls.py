90550ccb56afda04599925d2239b57d13b6567ff
Matt Westcott <matt@west.co.tt>
2013-09-02 13:12:28 +0100

    recognise dashes in page slugs

--- django-verdant/core/urls.py
+++ django-verdant/core/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('core.views',
     # a '/'. If a trailing slash is not present, we leave CommonMiddleware to
     # handle it as usual (i.e. redirect it to the trailing slash version if
     # settings.APPEND_SLASH is True)
-    url(r'^((?:\w+/)*)$', 'serve' )
+    url(r'^((?:[\w\-]+/)*)$', 'serve' )
 )
