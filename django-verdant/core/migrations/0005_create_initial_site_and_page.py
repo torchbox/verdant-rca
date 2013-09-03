4d371abbadcb5c5eb14d0393d615af9f366a39e2
Matt Westcott <matt@west.co.tt>
2013-09-03 13:52:17 +0100

    apparently initial_data is a bad idea (because syncdb and migrate will clobber any updates to it). Use a data migration instead.

---
+++ django-verdant/core/migrations/0005_create_initial_site_and_page.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        page_content_type = orm['contenttypes.ContentType'].objects.get(app_label='core', model='page')
+
+        homepage = orm['core.Page'].objects.create(
+            title="Welcome to your new Verdant site!",
+            slug="index",
+            content_type=page_content_type,
+        )
+        orm['core.Site'].objects.create(hostname="*", root_page=homepage)
+
+    def backwards(self, orm):
+        orm['core.Site'].objects.delete()
+        orm['core.Page'].objects.delete()
+
+    models = {
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'core.page': {
+            'Meta': {'unique_together': "(('parent', 'slug'),)", 'object_name': 'Page'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'pages'", 'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'subpages'", 'null': 'True', 'to': u"orm['core.Page']"}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'core.site': {
+            'Meta': {'object_name': 'Site'},
+            'hostname': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'root_page': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Page']"})
+        }
+    }
+
+    complete_apps = ['core']
+    symmetrical = True
