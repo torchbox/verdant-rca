a9c2893be2f7f9fda7c808f71e446b410a29da67
Matt Westcott <matt@west.co.tt>
2013-09-12 12:06:17 +0100

    add helper module for rendering modal-workflow responses, and port image chooser to it

--- django-verdant/verdantimages/views.py
+++ django-verdant/verdantimages/views.py
@@ -1,23 +1,17 @@
-from django.http import HttpResponse
-from django.template.loader import render_to_string
 from django.shortcuts import get_object_or_404
 
 import json
 
+from verdantadmin.modal_workflow import render_modal_workflow
 from verdantimages.models import Image
 
 def chooser(request):
     images = Image.objects.order_by('title')
 
-    html = render_to_string('verdantimages/chooser/chooser.html', {'images': images})
-    js = render_to_string('verdantimages/chooser/chooser.js', {'images': images})
-
-    return HttpResponse("""
-        {
-            'html': %s,
-            'onload': %s
-        }
-    """ % (json.dumps(html), js), mimetype="text/javascript")
+    return render_modal_workflow(
+        request, 'verdantimages/chooser/chooser.html', 'verdantimages/chooser/chooser.js',
+        {'images': images}
+    )
 
 
 def image_chosen(request, image_id):
@@ -35,10 +29,7 @@ def image_chosen(request, image_id):
         }
     })
 
-    js = render_to_string('verdantimages/chooser/image_chosen.js', {'image_json': image_json})
-
-    return HttpResponse("""
-        {
-            'onload': %s
-        }
-    """ % js, mimetype="text/javascript")
+    return render_modal_workflow(
+        request, None, 'verdantimages/chooser/image_chosen.js',
+        {'image_json': image_json}
+    )
