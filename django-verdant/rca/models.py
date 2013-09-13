5765745b53239f82e072182275062e77ef600afe
Matt Westcott <matt@west.co.tt>
2013-09-13 15:24:07 +0100

    Add author pages as another page type to play with

--- django-verdant/rca/models.py
+++ django-verdant/rca/models.py
@@ -25,17 +25,39 @@ class EditorialPage(Page):
     body = RichTextField()
 
 
+# == Authors Index ==
+class AuthorsIndex(Page):
+    subpage_types = ['AuthorPage']
+
+
+# == Author Page ==
+class AuthorPage(EditorialPage):
+    mugshot = models.ForeignKey('verdantimages.Image', null=True, blank=True, related_name='+')
+
+class AuthorPageAdmin(AdminHandler):
+    model = AuthorPage
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('slug'),
+        ImageChooserPanel('mugshot'),
+        RichTextFieldPanel('body'),
+    ]
+
+register(AuthorPage, AuthorPageAdmin)
+
+# == News Index ==
 class NewsIndex(Page):
     subpage_types = ['NewsItem']
 
 
+# == News Item ==
 class NewsItem(EditorialPage):
+    author = models.ForeignKey('rca.AuthorPage', null=True, blank=True, related_name='news_items')
     lead_image = models.ForeignKey('verdantimages.Image', null=True, blank=True, related_name='+')
-
 
 class NewsItemRelatedLink(RelatedLink):
     news_item = models.ForeignKey('NewsItem', related_name='related_links')
-
 
 class NewsItemAdminHandler(AdminHandler):
     model = NewsItem
@@ -45,6 +67,7 @@ class NewsItemAdminHandler(AdminHandler):
     panels = [
         FieldPanel('title'),
         FieldPanel('slug'),
+        FieldPanel('author'),
         ImageChooserPanel('lead_image'),
         RichTextFieldPanel('body'),
         InlinePanel(NewsItem, NewsItemRelatedLink, label="Wonderful related links",
@@ -53,6 +76,5 @@ class NewsItemAdminHandler(AdminHandler):
             panels=[FieldPanel('url'), FieldPanel('link_text'), ImageChooserPanel('image')]
         ),
     ]
-
 
 register(NewsItem, NewsItemAdminHandler)
