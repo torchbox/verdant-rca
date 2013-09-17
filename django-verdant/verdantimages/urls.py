1a035c12f198cd9d6888b17c1d9418c6a890a849
Matt Westcott <matt@west.co.tt>
2013-09-17 16:51:14 +0100

    image edit form

--- django-verdant/verdantimages/urls.py
+++ django-verdant/verdantimages/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('verdantimages.views',
     url(r'^$', 'images.index', name='verdantimages_index'),
+    url(r'^(\d+)/$', 'images.edit', name='verdantimages_edit_image'),
 
     url(r'^chooser/$', 'chooser.chooser', name='verdantimages_chooser'),
     url(r'^chooser/(\d+)/$', 'chooser.image_chosen', name='verdantimages_image_chosen'),
--- django-verdant/verdantimages/views/images.py
+++ django-verdant/verdantimages/views/images.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 
 from verdantimages.models import Image
+from verdantimages.forms import EditImageForm
 
 def index(request):
     images = Image.objects.order_by('title')
 
     return render(request, "verdantimages/images/index.html", {
         'images': images,
+    })
+
+
+def edit(request, image_id):
+    image = get_object_or_404(Image, id=image_id)
+
+    if request.POST:
+        form = EditImageForm(request.POST, instance=image)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Image '%s' updated." % image.title)
+            return redirect('verdantimages_index')
+
+    else:
+        form = EditImageForm(instance=image)
+
+    return render(request, "verdantimages/images/edit.html", {
+        'image': image,
+        'form': form,
     })
