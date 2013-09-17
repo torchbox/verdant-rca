3f20274ea06aa49fca58819c9307b4283b4d90d1
Matt Westcott <matt@west.co.tt>
2013-09-17 17:01:23 +0100

    add-image page

--- django-verdant/verdantimages/views/images.py
+++ django-verdant/verdantimages/views/images.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 
 from verdantimages.models import Image
-from verdantimages.forms import EditImageForm
+from verdantimages.forms import ImageForm, EditImageForm
 
 def index(request):
     images = Image.objects.order_by('title')
@@ -27,5 +27,21 @@ def edit(request, image_id):
 
     return render(request, "verdantimages/images/edit.html", {
         'image': image,
+        'form': form,
+    })
+
+
+def add(request):
+    if request.POST:
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save()
+            messages.success(request, "Image '%s' added." % image.title)
+            return redirect('verdantimages_index')
+
+    else:
+        form = ImageForm()
+
+    return render(request, "verdantimages/images/add.html", {
         'form': form,
     })
