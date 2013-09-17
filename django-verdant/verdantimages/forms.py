a85e6de5f4086b78f5f4ec8771808e87414a6881
Matt Westcott <matt@west.co.tt>
2013-09-17 15:53:11 +0100

    complete 'select format' interface for images in rich text, passing back a format name that's translated into class/filterspec at render time

--- django-verdant/verdantimages/forms.py
+++ django-verdant/verdantimages/forms.py
@@ -1,8 +1,21 @@
 from django import forms
 
 from verdantimages.models import Image
+from verdantimages.formats import FORMATS
 
 
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
+
+
+class ImageInsertionForm(forms.Form):
+    """
+    Form for selecting parameters of the image (e.g. format) prior to insertion
+    into a rich text area
+    """
+    format = forms.ChoiceField(
+        choices=[(format.name, format.label) for format in FORMATS],
+        widget=forms.RadioSelect
+    )
+    alt_text = forms.CharField()
--- django-verdant/verdantimages/views.py
+++ django-verdant/verdantimages/views.py
@@ -4,7 +4,8 @@ import json
 
 from verdantadmin.modal_workflow import render_modal_workflow
 from verdantimages.models import Image
-from verdantimages.forms import ImageForm
+from verdantimages.forms import ImageForm, ImageInsertionForm
+from verdantimages.formats import FORMATS_BY_NAME
 
 
 def get_image_json(image):
@@ -74,7 +75,33 @@ def chooser_upload(request):
 def chooser_select_format(request, image_id):
     image = get_object_or_404(Image, id=image_id)
 
+    if request.POST:
+        form = ImageInsertionForm(request.POST, initial={'alt_text': image.title})
+        if form.is_valid():
+
+            format = FORMATS_BY_NAME[form.cleaned_data['format']]
+            preview_image = image.get_rendition(format.filter_spec)
+
+            image_json = json.dumps({
+                'id': image.id,
+                'title': image.title,
+                'format': format.name,
+                'alt': form.cleaned_data['alt_text'],
+                'preview': {
+                    'url': preview_image.url,
+                    'width': preview_image.width,
+                    'height': preview_image.height,
+                }
+            })
+
+            return render_modal_workflow(
+                request, None, 'verdantimages/chooser/image_chosen.js',
+                {'image_json': image_json}
+            )
+    else:
+        form = ImageInsertionForm(initial={'alt_text': image.title})
+
     return render_modal_workflow(
         request, 'verdantimages/chooser/select_format.html', 'verdantimages/chooser/select_format.js',
-        {'image': image}
+        {'image': image, 'form': form}
     )
