45f813db766bcf7062ea0737b19b4f490d8d9a20
Matt Westcott <matt@west.co.tt>
2013-09-18 15:27:17 +0100

    install django-taggit

--- django-verdant/verdant/settings/base.py
+++ django-verdant/verdant/settings/base.py
@@ -138,6 +138,7 @@ INSTALLED_APPS = (
     'compressor',
     'debug_toolbar',
     'treebeard',
+    'taggit',
 
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
