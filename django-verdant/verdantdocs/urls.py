e423f328f7d568a8ddca2af37e9e957bdf224513
Matt Westcott <matt@west.co.tt>
2013-09-23 11:18:07 +0100

    Add 'add document' view

--- django-verdant/verdantdocs/urls.py
+++ django-verdant/verdantdocs/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('verdantdocs.views',
     url(r'^$', 'documents.index', name='verdantdocs_index'),
+    url(r'^add/$', 'documents.add', name='verdantdocs_add_document'),
 )
