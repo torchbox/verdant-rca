1f6f274ae18c6cba2fab67aecdeec4373859e628
Nick Smith <nick.smith@torchbox.com>
2013-09-24 11:36:13 +0100

    Add Taggit to models.Document

--- django-verdant/verdantdocs/models.py
+++ django-verdant/verdantdocs/models.py
@@ -2,9 +2,14 @@ from django.db import models
 from django.db.models.signals import pre_delete
 from django.dispatch.dispatcher import receiver
 
+from taggit.managers import TaggableManager
+from taggit.models import Tag
+
 class Document(models.Model):
     title = models.CharField(max_length=255)
     file = models.FileField(upload_to='documents')
+
+    tags = TaggableManager(help_text=None)
 
     def __unicode__(self):
         return self.title
