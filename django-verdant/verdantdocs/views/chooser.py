3dc8c64cf8a8d1af73416dead09277325e55d72a
Nick Smith <nick.smith@torchbox.com>
2013-09-24 15:36:39 +0100

    Add document search functionality to modal

--- django-verdant/verdantdocs/views/chooser.py
+++ django-verdant/verdantdocs/views/chooser.py
@@ -1,17 +1,28 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
 import json
 
 from verdantadmin.modal_workflow import render_modal_workflow
 from verdantdocs.models import Document
+from verdantdocs.forms import DocumentForm
+from verdantadmin.forms import SearchForm
 
 
 def chooser(request):
-    documents = Document.objects.order_by('title')
+    uploadform = DocumentForm()
+    documents = []
+    if 'q' in request.GET:
+        searchform = SearchForm(request.GET)
+        if searchform.is_valid():
+            q = searchform.cleaned_data['q']
+            documents = Document.search(q)
+        return render(request, "verdantdocs/chooser/search_results.html", {'documents': documents})
+    else:
+        searchform = SearchForm()
 
     return render_modal_workflow(
         request, 'verdantdocs/chooser/chooser.html', 'verdantdocs/chooser/chooser.js',
-        {'documents': documents}
+        {'documents': documents, 'uploadform': uploadform, 'searchform': searchform,}
     )
 
 
@@ -23,4 +34,26 @@ def document_chosen(request, document_id):
     return render_modal_workflow(
         request, None, 'verdantdocs/chooser/document_chosen.js',
         {'document_json': document_json}
+    )
+
+
+def chooser_upload(request):
+    if request.POST:
+        form = DocumentForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            document = form.save()
+            document_json = json.dumps({'id': document.id, 'title': document.title})
+            return render_modal_workflow(
+                request, None, 'verdantdocs/chooser/document_chosen.js',
+                {'document_json': document_json}
+            )
+    else:
+        form = DocumentForm()
+
+    documents = Document.objects.order_by('title')
+
+    return render_modal_workflow(
+        request, 'verdantdocs/chooser/chooser.html', 'verdantdocs/chooser/chooser.js',
+        {'documents': documents, 'uploadform': form}
     )
