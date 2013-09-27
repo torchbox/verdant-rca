0684b14f6358ddecffef604a1b59f9e15e466612
Matt Westcott <matt@west.co.tt>
2013-09-27 14:17:11 +0100

    pass 'request' in the context for the upcoming_events tag, so that we can use the pageurl tag inside it

--- django-verdant/rca/templatetags/rca_tags.py
+++ django-verdant/rca/templatetags/rca_tags.py
@@ -5,9 +5,10 @@ from datetime import date
 
 register = template.Library()
 
-@register.inclusion_tag('rca/includes/modules/upcoming_events.html')
-def upcoming_events(count=3):
+@register.inclusion_tag('rca/includes/modules/upcoming_events.html', takes_context=True)
+def upcoming_events(context, count=3):
     events = EventItem.objects.filter(date_to__gte=date.today()).order_by('date_from')[:count]
     return {
-        'events': events
+        'events': events,
+        'request': context['request'],  # required by the {% pageurl %} tag that we want to use within this template
     }
