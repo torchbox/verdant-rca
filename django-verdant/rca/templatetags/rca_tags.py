036253611342f1da6bad12fc24059cea1d8b9843
Helen C <helen.chapman@torchbox.com>
2013-09-30 09:33:04 +0100

    more work on events and standard index

--- django-verdant/rca/templatetags/rca_tags.py
+++ django-verdant/rca/templatetags/rca_tags.py
@@ -12,3 +12,11 @@ def upcoming_events(context, count=3):
         'events': events,
         'request': context['request'],  # required by the {% pageurl %} tag that we want to use within this template
     }
+
+@register.inclusion_tag('rca/includes/modules/carousel-news.html', takes_context=True)
+def news_carousel(context, count=6):
+    news_items = NewsItem.objects.filter(area=self.news_carousel_area)[:count]
+    return {
+        'news_items': news_items,
+        'request': context['request'],  # required by the {% pageurl %} tag that we want to use within this template
+    }
