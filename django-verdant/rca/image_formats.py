e05d3dfa19aba9e07536a577cf570bf7e9d37809
Matt Westcott <matt@west.co.tt>
2013-10-02 15:40:19 +0100

    Implement CaptionedImageFormat subclass of ImageFormat for use on RCA

--- django-verdant/rca/image_formats.py
+++ django-verdant/rca/image_formats.py
@@ -1,3 +1,33 @@
-from verdantimages.formats import Format, register_image_format
+from verdantimages.formats import Format, unregister_image_format, register_image_format
+from django.utils.html import escape
 
-register_image_format(Format('thumb', 'Thumbnail', 'thumb', 'fill-130x100'))
+class CaptionedImageFormat(Format):
+    def image_to_html(self, image, alt_text, extra_attributes):
+        rendition = image.get_rendition(self.filter_spec)
+
+        if self.classnames:
+            class_attr = 'class="%s" ' % escape(self.classnames)
+        else:
+            class_attr = ''
+
+        caption = "%s. %s, %s" % (image.title, image.creator, image.year)
+
+        return """<figure %s%s>
+            <img src="%s" width="%d" height="%d" alt="%s" />
+            <figcaption class="a4">%s</figcaption>
+        </figure>""" % (
+            extra_attributes, class_attr,
+            escape(rendition.url), rendition.width, rendition.height, alt_text,
+            escape(caption)
+        )
+
+unregister_image_format('fullwidth')
+# unregister_image_format('left')
+# unregister_image_format('right')
+
+register_image_format(
+    CaptionedImageFormat('fullwidth', 'Full width', 'bodytext-image', 'fill-304x171')
+)
+register_image_format(
+    CaptionedImageFormat('halfwidthright', 'Half width right aligned', 'bodytext-image small', 'fill-304x171')
+)
