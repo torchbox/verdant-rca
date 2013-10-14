06fa2f3b10a6eb924f9622155fae70d3c4d8b3dc
Helen C <helen.chapman@torchbox.com>
2013-10-14 20:17:53 +0100

    more work on event index

--- django-verdant/rca/static/rca/js/events-listing.js
+++ django-verdant/rca/static/rca/js/events-listing.js
@@ -1,6 +1,13 @@
 $(function(){
-	$('#programme-listing-filter').submit(function() {
-		$('#listing').load("/events/", $(this).serialize());
-		return false;
+	$('#filters input[type="radio"]').change(function() {
+		// other fields are a special case - need to allow user to fill in
+		//the text field
+		if ($(this).val() == 'other') {
+			
+		} else {
+			$('#listing').load("/events/", $('#filters').serialize());
+			$(this).closest('li').removeClass('expanded');
+			return false;
+		}
 	})
 });
