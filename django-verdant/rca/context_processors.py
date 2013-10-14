063b476da796e0ff01f3aa1d0a6c6099f7565826
Helen C <helen.chapman@torchbox.com>
2013-10-14 13:01:10 +0100

    further work on events index - change form fields to radio buttons and pagination

--- django-verdant/rca/context_processors.py
+++ django-verdant/rca/context_processors.py
@@ -1,6 +1,11 @@
-from rca.models import Advert
+from rca.models import Advert, SCHOOL_CHOICES, PROGRAMME_CHOICES, EVENT_LOCATION_CHOICES, AREA_CHOICES, EVENT_AUDIENCE_CHOICES
 
 def global_vars(request):
 	return {
 		'global_adverts': Advert.objects.filter(show_globally=True),
+		'global_schools': SCHOOL_CHOICES,
+		'global_programmes': PROGRAMME_CHOICES,
+		'global_locations': EVENT_LOCATION_CHOICES,
+		'global_areas': AREA_CHOICES,
+		'global_audiences': EVENT_AUDIENCE_CHOICES,
 	}
