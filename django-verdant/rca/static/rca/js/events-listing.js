bf11fac0536cebee76d9053ff864e1f9d9dfb9b4
Helen C <helen.chapman@torchbox.com>
2013-10-15 10:35:01 +0100

    setting variable for events index url outside the events_listing.js file so it can be used within it

--- django-verdant/rca/static/rca/js/events-listing.js
+++ django-verdant/rca/static/rca/js/events-listing.js
@@ -1,11 +1,12 @@
 $(function(){
+	console.log(events_index);
 	$('#filters input[type="radio"]').change(function() {
 		// other fields are a special case - need to allow user to fill in
 		//the text field
 		if ($(this).val() == 'other') {
 			
 		} else {
-			$('#listing').load("/events/", $('#filters').serialize());
+			$('#listing').load(events_index, $('#filters').serialize());
 			$(this).closest('li').removeClass('expanded');
 			return false;
 		}
