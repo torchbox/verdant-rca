afa31c8eee74ba8c1c6032c847c03ad633931f6e
Helen C <helen.chapman@torchbox.com>
2013-10-16 12:00:54 +0100

    redo events listing with select boxes

--- django-verdant/rca/static/rca/js/events-listing.js
+++ django-verdant/rca/static/rca/js/events-listing.js
@@ -1,14 +1,7 @@
 $(function(){
-	console.log(events_index);
-	$('#filters input[type="radio"]').change(function() {
-		// other fields are a special case - need to allow user to fill in
-		//the text field
-		if ($(this).val() == 'other') {
-			
-		} else {
-			$('#listing').load(events_index, $('#filters').serialize());
-			$(this).closest('li').removeClass('expanded');
-			return false;
-		}
+	$('#filters .options li').click(function() {
+		$('#listing').load(events_index, $('#filters').serialize());
+		$(this).parent().closest('li').removeClass('expanded');
+		return false;
 	});
 });
