6b9daba0c72b9343837161f32427c1ddd0a9869b
Dave Cranwell <david@torchbox.com>
2013-10-17 09:27:50 +0100

    ongoing temporary tweaks to allow templates to design user interfaces

--- django-verdant/verdant/urls.py
+++ django-verdant/verdant/urls.py
@@ -10,6 +10,7 @@ from verdantimages import urls as verdantimages_urls
 from verdantdocs import admin_urls as verdantdocs_admin_urls
 from verdantdocs import urls as verdantdocs_urls
 from verdantsnippets import urls as verdantsnippets_urls
+from verdantusers import urls as verdantusers_urls
 
 from rca import app_urls
 
