253397bd7ee46a3f9f5a9a80fd3d4c96d1829f80
Dave Cranwell <david@torchbox.com>
2013-10-22 17:21:02 +0100

    added menu sliding

--- django-verdant/rca/static/rca/js/nav.js
+++ django-verdant/rca/static/rca/js/nav.js
@@ -1,22 +1,12 @@
 $(function(){
 
-	$(".mobile-menu-button").click(function(){
-		if($(".mobile-menu-wrapper").width() === 0){
-			$(".mobile-content-wrapper").width($(".mobile-content-wrapper").width() + "px");
-			$("body")
-				.css({
-					"width": "150%",
-					"overflow-x": "hidden"
-				})
-				.addClass("show-mobile-menu");
-		}else{
-			$("body").removeClass("show-mobile-menu");
-			setTimeout(function(){
-				$("body, .mobile-content-wrapper").removeAttr("style");
-			}, 1100);
-		}
-		return false;
+	/* configure left slideout menu toggle */
+	$(".mobile-menu-button").toggleClick(function(){
+		$("body").addClass("show-mobile-menu");
+	},function(){
+		$("body").removeClass("show-mobile-menu");
 	});
+
 
 	// copy the sidebar so that we can show it on the left in the mobile version
 	if(!$(".mobile-menu-wrapper > aside").length){
@@ -59,9 +49,9 @@ $(function(){
 	});
 
 	/* IE<9 targetted execution of above desktopSmall Harvey stuff, since media queries aren't understood */
-	$('.lt-ie9').each(function(){
-		desktopNav.apply()
-	})
+    $('.lt-ie9').each(function(){
+        desktopNav.apply()
+    })
 
 });
 
