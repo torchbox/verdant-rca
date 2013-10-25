9556f1fd36a7cc6564cffd25ef658e1b4a00f7af
Matt Westcott <matt@west.co.tt>
2013-10-25 11:32:24 +0100

    Filter front-end queries in rca_tags to only include live pages

--- django-verdant/rca/templatetags/rca_tags.py
+++ django-verdant/rca/templatetags/rca_tags.py
@@ -11,7 +11,7 @@ register = template.Library()
 
 @register.inclusion_tag('rca/tags/upcoming_events.html', takes_context=True)
 def upcoming_events(context, exclude=None, count=3):
-    events = EventItem.future_objects.annotate(start_date=Min('dates_times__date_from')).order_by('start_date')
+    events = EventItem.future_objects.filter(live=True).annotate(start_date=Min('dates_times__date_from')).order_by('start_date')
     if exclude:
         events = events.exclude(id=exclude.id)
     return {
@@ -22,11 +22,11 @@ def upcoming_events(context, exclude=None, count=3):
 @register.inclusion_tag('rca/tags/carousel_news.html', takes_context=True)
 def news_carousel(context, area="", programme="", school="", count=6):
     if area:
-        news_items = NewsItem.objects.filter(area=area)[:count]
+        news_items = NewsItem.objects.filter(live=True, area=area)[:count]
     elif programme:
-        news_items = NewsItem.objects.filter(related_programmes__programme=programme)[:count]
+        news_items = NewsItem.objects.filter(live=True, related_programmes__programme=programme)[:count]
     elif school:
-        news_items = NewsItem.objects.filter(related_schools__school=school)[:count]
+        news_items = NewsItem.objects.filter(live=True, related_schools__school=school)[:count]
     else:
         # neither programme nor area specified - return no results
         news_items = NewsItem.objects.none()
@@ -39,9 +39,9 @@ def news_carousel(context, area="", programme="", school="", count=6):
 @register.inclusion_tag('rca/tags/upcoming_events_related.html', takes_context=True)
 def upcoming_events_related(context, opendays=0, programme="", school="", display_name="", events_index_url="/events/"):
     if school:
-        events = EventItem.future_objects.annotate(start_date=Min('dates_times__date_from')).filter(related_schools__school=school).order_by('start_date')
+        events = EventItem.future_objects.filter(live=True).annotate(start_date=Min('dates_times__date_from')).filter(related_schools__school=school).order_by('start_date')
     elif programme:
-        events = EventItem.future_objects.annotate(start_date=Min('dates_times__date_from')).filter(related_programmes__programme=programme).order_by('start_date')
+        events = EventItem.future_objects.filter(live=True).annotate(start_date=Min('dates_times__date_from')).filter(related_programmes__programme=programme).order_by('start_date')
     if opendays:
         events = events.filter(audience='openday')
     else:
@@ -58,7 +58,7 @@ def upcoming_events_related(context, opendays=0, programme="", school="", displa
 
 @register.inclusion_tag('rca/tags/programmes_by_school.html', takes_context=True)
 def programme_by_school(context, school):
-    programmes = ProgrammePage.objects.filter(school=school)
+    programmes = ProgrammePage.objects.filter(live=True, school=school)
     return {
         'programmes': programmes,
         'request': context['request'],  # required by the {% pageurl %} tag that we want to use within this template
@@ -66,7 +66,7 @@ def programme_by_school(context, school):
 
 @register.inclusion_tag('rca/tags/staff_by_programme.html', takes_context=True)
 def staff_by_programme(context, programme):
-    staff = StaffPage.objects.filter(roles__programme=programme)
+    staff = StaffPage.objects.filter(live=True, roles__programme=programme)
     return {
         'staff': staff,
         'request': context['request'],  # required by the {% pageurl %} tag that we want to use within this template
@@ -74,7 +74,7 @@ def staff_by_programme(context, programme):
 
 @register.inclusion_tag('rca/tags/alumni_by_programme.html', takes_context=True)
 def alumni_by_programme(context, programme):
-    alumni = AlumniPage.objects.filter(programme=programme)
+    alumni = AlumniPage.objects.filter(live=True, programme=programme)
     return {
         'alumni': alumni,
         'request': context['request'],  # required by the {% pageurl %} tag that we want to use within this template
@@ -83,9 +83,9 @@ def alumni_by_programme(context, programme):
 @register.inclusion_tag('rca/tags/rca_now_related.html', takes_context=True)
 def rca_now_related(context, programme="", author=""):
     if programme:
-        rcanow = RcaNowPage.objects.filter(show_on_homepage=1).filter(programme=programme)
+        rcanow = RcaNowPage.objects.filter(live=True, show_on_homepage=1).filter(programme=programme)
     elif author:
-        rcanow = RcaNowPage.objects.filter(author=author)
+        rcanow = RcaNowPage.objects.filter(live=True, author=author)
     return {
         'rcanow_pages': rcanow,
         'request': context['request'],  # required by the {% pageurl %} tag that we want to use within this template
@@ -94,11 +94,11 @@ def rca_now_related(context, programme="", author=""):
 @register.inclusion_tag('rca/tags/research_related.html', takes_context=True)
 def research_related(context, programme="", person="", school="", exclude=None):
     if programme:
-        research_items = ResearchItem.objects.filter(programme=programme)
+        research_items = ResearchItem.objects.filter(live=True, programme=programme)
     elif person:
-        research_items = ResearchItem.objects.filter(creator__person=person)
+        research_items = ResearchItem.objects.filter(live=True, creator__person=person)
     elif school:
-        research_items = ResearchItem.objects.filter(school=school)
+        research_items = ResearchItem.objects.filter(live=True, school=school)
     if exclude:
         research_items = research_items.exclude(id=exclude.id)
     return {
@@ -111,7 +111,7 @@ def research_related(context, programme="", person="", school="", exclude=None):
 
 @register.inclusion_tag('rca/tags/rca_now_latest.html', takes_context=True)
 def rca_now_latest(context, exclude=None, count=4):
-    rcanow = RcaNowPage.objects.all()
+    rcanow = RcaNowPage.objects.filter(live=True)
     if exclude:
         rcanow = rcanow.exclude(id=exclude.id)
     rcanow = rcanow.order_by('date')
@@ -122,7 +122,7 @@ def rca_now_latest(context, exclude=None, count=4):
 
 @register.inclusion_tag('rca/tags/upcoming_jobs.html', takes_context=True)
 def upcoming_jobs(context, exclude=None, count=6):
-    jobs = JobPage.objects.filter(closing_date__gte=date.today()).order_by('closing_date')
+    jobs = JobPage.objects.filter(live=True, closing_date__gte=date.today()).order_by('closing_date')
     if exclude:
         jobs = jobs.exclude(id=exclude.id)
     return {
@@ -132,7 +132,7 @@ def upcoming_jobs(context, exclude=None, count=6):
 
 @register.inclusion_tag('rca/tags/jobs_listing.html', takes_context=True)
 def jobs_listing(context):
-    jobs = JobPage.objects.filter(closing_date__gte=date.today()).order_by('closing_date')
+    jobs = JobPage.objects.filter(live=True, closing_date__gte=date.today()).order_by('closing_date')
     return {
         'jobs': jobs,
         'request': context['request'],  # required by the {% pageurl %} tag that we want to use within this template
@@ -140,7 +140,7 @@ def jobs_listing(context):
 
 @register.inclusion_tag('rca/tags/students_related.html', takes_context=True)
 def students_related(context, programme="", year="", exclude=None, count=4):
-    students = StudentPage.objects.filter(programme=programme)
+    students = StudentPage.objects.filter(live=True, programme=programme)
     students = students.filter(degree_year=year)
     if exclude:
         students = students.exclude(id=exclude.id)
@@ -152,7 +152,7 @@ def students_related(context, programme="", year="", exclude=None, count=4):
 # Queries students who 'have work' (i.e. have some carousel entries). Also matches degree year
 @register.inclusion_tag('rca/tags/students_related_work.html', takes_context=True)
 def students_related_work(context, year="", exclude=None, count=4):
-    students = StudentPage.objects.filter(degree_year=year)
+    students = StudentPage.objects.filter(live=True, degree_year=year)
     students = students.filter(carousel_items__image__isnull=False) | students.filter(carousel_items__embedly_url__isnull=False)
     students=students.distinct()
 
@@ -165,7 +165,7 @@ def students_related_work(context, year="", exclude=None, count=4):
 
 @register.inclusion_tag('rca/tags/staff_random.html', takes_context=True)
 def staff_random(context, exclude=None, count=4):
-    staff = StaffPage.objects.all().order_by('?')
+    staff = StaffPage.objects.filter(live=True).order_by('?')
     if exclude:
         staff = staff.exclude(id=exclude.id)
     return {
@@ -175,13 +175,13 @@ def staff_random(context, exclude=None, count=4):
 
 @register.inclusion_tag('rca/tags/homepage_packery.html', takes_context=True)
 def homepage_packery(context, news_count=5, staff_count=5, student_count=5, tweets_count=5, rcanow_count=5, standard_count=5, research_count=5, alumni_count=5):
-    news = NewsItem.objects.filter(show_on_homepage=1).order_by('?')
-    staff = StaffPage.objects.filter(show_on_homepage=1).order_by('?')
-    student = StudentPage.objects.filter(show_on_homepage=1).order_by('?')
-    rcanow = RcaNowPage.objects.filter(show_on_homepage=1).order_by('?')
-    standard = StandardPage.objects.filter(show_on_homepage=1).order_by('?')
-    research = ResearchItem.objects.filter(show_on_homepage=1).order_by('?')
-    alumni = AlumniPage.objects.filter(show_on_homepage=1).order_by('?')
+    news = NewsItem.objects.filter(live=True, show_on_homepage=1).order_by('?')
+    staff = StaffPage.objects.filter(live=True, show_on_homepage=1).order_by('?')
+    student = StudentPage.objects.filter(live=True, show_on_homepage=1).order_by('?')
+    rcanow = RcaNowPage.objects.filter(live=True, show_on_homepage=1).order_by('?')
+    standard = StandardPage.objects.filter(live=True, show_on_homepage=1).order_by('?')
+    research = ResearchItem.objects.filter(live=True, show_on_homepage=1).order_by('?')
+    alumni = AlumniPage.objects.filter(live=True, show_on_homepage=1).order_by('?')
     tweets = [[],[],[],[],[]]
 
     packeryItems =list(chain(news[:news_count], staff[:staff_count], student[:student_count], rcanow[:rcanow_count], standard[:standard_count], research[:research_count], alumni[:alumni_count], tweets[:tweets_count]))
@@ -195,7 +195,7 @@ def homepage_packery(context, news_count=5, staff_count=5, student_count=5, twee
 @register.inclusion_tag('rca/tags/sidebar_links.html', takes_context=True)
 def sidebar_links(context, calling_page=None):
     if calling_page:
-        pages = calling_page.get_children().filter(show_in_menus=True)
+        pages = calling_page.get_children().filter(live=True, show_in_menus=True)
     return {
         'pages': pages,
         'calling_page': calling_page, # needed to get related links from the tag
@@ -204,7 +204,7 @@ def sidebar_links(context, calling_page=None):
 
 @register.inclusion_tag('rca/tags/research_students_feed.html', takes_context=True)
 def research_students_feed(context, staff_page=None):
-    students = StudentPage.objects.filter(supervisor=staff_page)
+    students = StudentPage.objects.filter(live=True, supervisor=staff_page)
     return {
         'students': students,
         'request': context['request'],  # required by the {% pageurl %} tag that we want to use within this template
@@ -212,7 +212,7 @@ def research_students_feed(context, staff_page=None):
 
 @register.inclusion_tag('rca/tags/research_students_list.html', takes_context=True)
 def research_students_list(context, staff_page=None):
-    students = StudentPage.objects.filter(supervisor=staff_page)
+    students = StudentPage.objects.filter(live=True, supervisor=staff_page)
     return {
         'students': students,
         'staff_page': staff_page, #needed to get the supervised_student_other field to list research students without profile pages
