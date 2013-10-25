b11942ca234473abaef90f00385c97fb57a11f72
Karl Hobley <karl@torchbox.com>
2013-10-25 16:18:18 +0100

    Various changes to research importer

--- django-verdant/importer/research.py
+++ django-verdant/importer/research.py
@@ -35,13 +35,32 @@ WORK_TYPES_CHOICES = {
 }
 
 
+DIVISION_SCHOOL_MAPPING = {
+    "s1": "schoolofarchitecture",
+    "s2": "schoolofcommunication",
+    "s3": "schoolofdesign",
+    "s4": "schoolofmaterial",
+    "s5": "schooloffineart",
+    "s6": "schoolofhumanities",
+}
+
+
+def text_to_html(text):
+    paragraphs = text.split("\r\n")
+    html = ""
+    for paragraph in paragraphs:
+        html = "".join([html, "<p>", paragraph, "</p>"])
+    return html
+
+
 class ResearchImporter(object):
-    def __init__(self, staff_index, research_index, **kwargs):
-        self.staff_index = staff_index
-        self.research_index = research_index
-        self.cache_directory = kwargs.get("cache_directory", "importer/data/research/")
+    def __init__(self, **kwargs):
         self.save = kwargs.get("save", True)
+        self.cache_directory = kwargs.get("cache_directory", "importer/data/research/")
         self.research_cache_directory = self.cache_directory + "research/"
+        self.student_index = kwargs.get("student_index", "students")
+        self.research_index = kwargs.get("research_index", "current-research")
+        self.staff_index = kwargs.get("staff_index", "staff")
         self.http = httplib2.Http()
 
         # Create cache directories
@@ -50,30 +69,70 @@ class ResearchImporter(object):
         except OSError: # Directory alredy exists
             pass
 
-    def import_researchitem(self, staffpage, researchitem):
+
+    def find_person_page(self, person_name):
+        # Get list of potential names
+        names = [title + " " + person_name for title in ["Dr", "Professor", "Sir"]]
+        names.append(person_name)
+
+        # Slugify them
+        slugs = map(lambda slug: slug.strip(" ").replace("'", "").lower().replace(" ", "-"), names)
+
+        # Search the staff pages
+        for slug in slugs:
+            try:
+                return self.staff_index_page.get_children().get(slug=slug).specific
+            except:
+                continue
+
+        # Search the student pages
+        for slug in slugs:
+            try:
+                return self.student_index_page.get_children().get(slug=slug).specific
+            except:
+                continue
+
+        return None
+
+
+    def add_researchitemcreator(self, researchitem, creator_name):
+        # Find creator page
+        creator_page = self.find_person_page(creator_name)
+
+        # Add research item creator
+        if creator_page is not None:
+            ResearchItemCreator.objects.get_or_create(page=researchitem, person=creator_page)
+        else:
+            ResearchItemCreator.objects.get_or_create(page=researchitem, manual_person_name=creator_name)
+
+    def import_researchitem(self, researchitem):
         # Get basic info
         researchitem_eprintid = researchitem["eprintid"]
         researchitem_title = researchitem["title"]
         researchitem_abstract = researchitem.get("abstract", "")
         researchitem_type = researchitem["type"]
         researchitem_department = researchitem.get("department", "")
+        researchitem_divisions = researchitem.get("divisions", [])
 
         # Get year
-        researchitem_year = ""
         if "date" in researchitem:
-            researchitem_date = researchitem["date"]
-            if isinstance(researchitem_date, basestring):
-                try:
-                    researchitem_year = str(parse_date(researchitem_date).year)
-                except AttributeError:
-                    researchitem_year = researchitem_date[0:4]
-            elif isinstance(researchitem_date, int):
-                researchitem_year = str(researchitem_date)
+            # First 4 characters are always the year
+            researchitem_year = str(researchitem["date"])[:4]
+        elif "datestamp" in researchitem:
+            # First 4 characters are always the year
+            researchitem_year = str(researchitem["datestamp"])[:4]
+        else:
+            print "NO DATE"
+            researchitem_year = ""
 
         # Get school
+        # TODO: Use division instead
         researchitem_school = ""
         if researchitem_department in constants.SCHOOLS:
             researchitem_school = constants.SCHOOLS[researchitem_department]
+
+        # Convert description to HTML
+        researchitem_abstract = text_to_html(researchitem_abstract)
 
         # Create researchitem page
         try:
@@ -83,6 +142,7 @@ class ResearchImporter(object):
 
         # Set values
         researchitempage.title = researchitem_title
+        researchitempage.ref = True
         researchitempage.year = researchitem_year
         researchitempage.description = researchitem_abstract
         researchitempage.work_type = WORK_TYPES_CHOICES[researchitem_type]
@@ -94,33 +154,13 @@ class ResearchImporter(object):
             if researchitempage.id:
                 researchitempage.save()
             else:
-                self.research_index.add_child(researchitempage)
+                self.research_index_page.add_child(researchitempage)
 
-            # Link to staff page
-            ResearchItemCreator.objects.get_or_create(page=researchitempage, person=staffpage)
+        for creator in researchitem["creators"]:
+            creator_name = creator["name"]["given"] + " " + creator["name"]["family"]
+            self.add_researchitemcreator(researchitempage, creator_name)
 
-    def find_staff_page(self, staff):
-        first_name = staff["givenName"]
-        last_name = staff["sn"]
-
-        # Get list of potential names
-        names = [title + " " + first_name + " " + last_name for title in ["Dr", "Professor", "Sir"]]
-        names.append(first_name + " " + last_name)
-
-        # Slugify them
-        slugs = map(lambda slug: slug.strip(" ").replace("'", "").lower().replace(" ", "-"), names)
-
-        # Search the database
-        for slug in slugs:
-            try:
-                staffpage = self.staff_index.get_children().get(slug=slug).specific
-                return staffpage
-            except:
-                continue
-
-        return None
-
-    def download_staff_research(self, username, filename):
+    def download_research(self, username, filename):
         url = "http://researchonline.rca.ac.uk/cgi/search/archive/simple/export_rca_JSON.js?screen=Search&dataset=archive&_action_export=1&output=JSON&exp=0%%7C1%%7C%%7Carchive%%7C-%%7Cq%%3A%%3AALL%%3AIN%%3A%(username)s%%7C-%%7C&n=&cache=" % {
             "username": username,
         }
@@ -133,52 +173,46 @@ class ResearchImporter(object):
         else:
             return False
 
-    def import_staff_research(self, staff):
-        # Ignore staff where ref is False
-        if staff["REF"] == "FALSE":
-            return
+    def import_research(self, user):
+        # Ignore users with REF=FALSE
+        #if user["REF"] == "FALSE":
+        #    return
 
         # Get username
-        staff_username = staff["sAMAccountName"]
+        username = user["sAMAccountName"]
 
-        # Get staff page
-        staffpage = self.find_staff_page(staff)
-
-        if staffpage is not None:
-            # Attempt to get research from cache
-            filename = self.research_cache_directory + staff_username + ".json"
-            try:
+        # Attempt to get research from cache
+        filename = self.research_cache_directory + username + ".json"
+        try:
+            f = open(filename, "r")
+        except IOError:
+            # Not in cache, download instead
+            if self.download_research(username, filename):
                 f = open(filename, "r")
-            except IOError:
+            else:
+                print "Unable to download research info for " + username
                 return
-                # Not in cache, download instead
-                #if self.download_staff_research(staff_username, filename):
-                #    f = open(filename, "r")
-                #else:
-                #    print "Unable to download research info for " + staff_username
-                #    return
 
-            # Load file
-            researchitems = json.loads(f.read())
+        # Load file
+        researchitems = json.loads(f.read())
 
-            # Get researchitems
-            print "Found " + str(len(researchitems)) + " research items for " + staff_username
-            for researchitem in researchitems:
-                self.import_researchitem(staffpage, researchitem)
-        else:
-            print "Could not find staff page for " + staff_username + ". Ignoring"
+        # Get researchitems
+        print "Found " + str(len(researchitems)) + " research items for " + username
+        for researchitem in researchitems:
+            self.import_researchitem(researchitem)
 
     def doimport(self, staff_data):
+        # Get index pages
+        self.student_index_page = Page.objects.get(slug=self.student_index).specific
+        self.research_index_page = Page.objects.get(slug=self.research_index).specific
+        self.staff_index_page = Page.objects.get(slug=self.staff_index).specific
+
         # Iterate through staff list
         for staff in staff_data:
-            self.import_staff_research(staff)
+            self.import_research(staff)
 
 
 def doimport():
-    # Get indexes
-    staff_index = StaffIndex.objects.get(slug="staff")
-    research_index = CurrentResearchPage.objects.get(slug="current-research")
-
     # Import
-    importer = ResearchImporter(staff_index, research_index, save=True)
+    importer = ResearchImporter(save=True)
     importer.doimport(staff_data)
