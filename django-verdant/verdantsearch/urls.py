ffa25509d6da7e188178d3eaf2e41d78542c04b0
Karl Hobley <karl@torchbox.com>
2013-10-28 13:11:55 +0000

    Added search frontend view

--- django-verdant/verdantsearch/urls.py
+++ django-verdant/verdantsearch/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 
 
-urlpatterns = patterns('verdantadmin.views',
-    url(r"^$", "views.search", name="verdantsearch_search"),
-    url(r"^suggest/$", "views.suggest", name="verdantsearch_suggest"),
+urlpatterns = patterns("verdantsearch.views",
+    url(r"^$", "search", name="verdantsearch_search"),
+    url(r"^suggest/$", "suggest", name="verdantsearch_suggest"),
 )
--- django-verdant/verdantsearch/views.py
+++ django-verdant/verdantsearch/views.py
@@ -1,21 +1,24 @@
-from verdantsearch import Search
+from django.conf import settings
+from django.shortcuts import render
+from core import models
 
 
 def search(request):
-    q = request.GET.get("q", "")
+    query_string = request.GET.get("q", "")
 
-    if q != "":
+    # Search
+    if query_string != "":
         do_search = True
-        search_results = Search().search(q)
+        search_results = models.Page.title_search(query_string)
     else:
         do_search = False
         search_results = None
 
+    # Get template
+    template_name = getattr(settings, "VERDANTSEARCH_RESULTS_TEMPLATE", "verdantsearch/search-results.html")
+
+    return render(request, template_name, dict(do_search=do_search, query_string=query_string, search_results=search_results))
+
 
 def suggest(request):
-    q = request.GET.get("q", "")
-
-    if q != "":
-        search_results = Search().search(q)
-    else:
-        search_results = None
+    query_string = request.GET.get("q", "")
