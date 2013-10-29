dee59b322831c772babbcf5346d054de089521b4
Matt Westcott <matt@west.co.tt>
2013-10-29 15:03:30 +0000

    add users index

--- django-verdant/verdantusers/urls.py
+++ django-verdant/verdantusers/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('verdantusers.views',
+    url(r'^$', 'users.index', name='verdantusers_index'),
     url(r'^logintest/$', 'users.logintest', name='verdantusers_logintest'),
 )
--- django-verdant/verdantusers/views/users.py
+++ django-verdant/verdantusers/views/users.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
+def index(request):
+    users = User.objects.all()
+
+    return render(request, 'verdantusers/index.html', {
+        'users': users,
+    })
 
 def logintest(request):
     return render(request, 'verdantusers/users/login.html', {})
