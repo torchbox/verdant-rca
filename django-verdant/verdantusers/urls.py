ba429a655f14e0b92d67964fb5a72165b3dd9527
Matt Westcott <matt@west.co.tt>
2013-10-29 15:27:45 +0000

    Add create-user page

--- django-verdant/verdantusers/urls.py
+++ django-verdant/verdantusers/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('verdantusers.views',
     url(r'^$', 'users.index', name='verdantusers_index'),
+    url(r'^new/$', 'users.create', name='verdantusers_create'),
     url(r'^logintest/$', 'users.logintest', name='verdantusers_logintest'),
 )
--- django-verdant/verdantusers/views/users.py
+++ django-verdant/verdantusers/views/users.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def index(request):
     users = User.objects.all()
 
     return render(request, 'verdantusers/index.html', {
         'users': users,
+    })
+
+def create(request):
+    if request.POST:
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, "User '%s' created." % user)
+            return redirect('verdantusers_index')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'verdantusers/create.html', {
+        'form': form
     })
 
 def logintest(request):
