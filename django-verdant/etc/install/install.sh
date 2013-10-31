4abf6526207853ca2e948ea44e26c5239e3435ab
Karl Hobley <karl@torchbox.com>
2013-10-31 14:35:24 +0000

    Installer now installs Java

--- django-verdant/etc/install/install.sh
+++ django-verdant/etc/install/install.sh
@@ -71,6 +71,7 @@ fi
 
 # ElasticSearch
 if ! command -v elasticsearch; then
+    apt-get install openjdk-6-jdk
     echo "Downloading ElasticSearch..."
     wget -q https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.5.deb
     dpkg -i elasticsearch-0.90.5.deb
