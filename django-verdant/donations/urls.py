b1377dd5a78fc9f698ee206d923e0e6b1d97c382
Balazs <balazs@torchbox.com>
2013-11-05 09:38:25 +0000

    Added donation export (only json for now)

--- django-verdant/donations/urls.py
+++ django-verdant/donations/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('donations.views',
     url(r'^$', 'donation', name='donation'),
+    url(r'^export/$', 'export', name='donations_export'),
 )
--- django-verdant/donations/views.py
+++ django-verdant/donations/views.py
@@ -1,12 +1,33 @@
 import simplejson
 import stripe
+import datetime
 
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .forms import DonationForm
 from .settings import STRIPE_PUBLISHABLE_KEY, STRIPE_SECRET_KEY
 
 stripe.api_key = STRIPE_SECRET_KEY
+
+
+def unique(seq, idfun=None):
+    # order preserving
+    if idfun is None:
+        def idfun(x):
+            return x
+    seen = {}
+    result = []
+    for item in seq:
+        marker = idfun(item)
+        # in old Python versions:
+        # if seen.has_key(marker)
+        # but in new ones:
+        if marker in seen:
+            continue
+        seen[marker] = 1
+        result.append(item)
+    return result
 
 
 def donation(request):
@@ -40,3 +61,37 @@ def donation(request):
         'form': form,
         'STRIPE_PUBLISHABLE_KEY': STRIPE_PUBLISHABLE_KEY,
     })
+
+
+def export(request):
+    # TODO: parse dates from query params
+    offset = request.POST.get("offset", 0)
+    count = request.POST.get("count", 100)
+    date_from = request.POST.get("date_from", datetime.datetime.now() - datetime.timedelta(days=31))
+    date_to = request.POST.get("date_to", datetime.datetime.now())
+    created = {
+        "gte": date_from.strftime("%s"),
+        "lte": date_to.strftime("%s"),
+    }
+
+    all_charges = []
+    charges_left = True
+
+    while charges_left:
+        # https://stripe.com/docs/api/python#list_charges
+        charges = stripe.Charge.all(count=count, offset=offset, created=created)['data']
+        if len(charges):
+            all_charges += charges
+            offset = offset + count
+        else:
+            charges_left = False
+
+    # if there was a charge made while retrieving the results there might be duplicates in the list
+    all_charges = unique(all_charges, lambda c: c["id"])
+
+    # covert objects to dicts, the ordering of keys and values is fixed:
+    # http://docs.python.org/2/library/stdtypes.html#dict.items
+    all_charges = [dict(zip(ch.keys(), ch.values())) for ch in all_charges]
+
+    # TODO: flatten json and convert to csv
+    return HttpResponse(simplejson.dumps(all_charges))
