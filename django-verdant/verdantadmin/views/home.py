b815c205c67288a6f438dd7e2ef7cb688caba3e9
Matt Westcott <matt@west.co.tt>
2013-11-07 14:57:10 +0000

    Show submitter on 'pages awaiting moderation' listing

--- django-verdant/verdantadmin/views/home.py
+++ django-verdant/verdantadmin/views/home.py
@@ -5,7 +5,7 @@ from core.models import PageRevision
 
 @login_required
 def home(request):
-    page_revisions_for_moderation = PageRevision.submitted_revisions.select_related('page').order_by('-created_at')
+    page_revisions_for_moderation = PageRevision.submitted_revisions.select_related('page', 'user').order_by('-created_at')
     return render(request, "verdantadmin/home.html", {
         'page_revisions_for_moderation': page_revisions_for_moderation
     })
