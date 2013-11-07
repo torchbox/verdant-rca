e9f34146508a217e5ed283864e29f51d0c2fb8d0
Balazs <balazs@torchbox.com>
2013-11-07 12:22:51 +0000

    Use console backend for emails + some celery related tweaks

--- django-verdant/verdant/settings/dev.py
+++ django-verdant/verdant/settings/dev.py
@@ -3,6 +3,8 @@ from .base import *
 DEBUG = True
 INSTALLED_APPS = list(INSTALLED_APPS) + ['devserver']
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 try:
     from .local import *
 except ImportError:
