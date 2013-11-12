4418107a7c61ce529bd26015104f88f7e2f16028
Dave Cranwell <david@torchbox.com>
2013-11-12 15:47:02 +0000

    ongoing tweaks

--- django-verdant/rca/static/rca/js/site.js
+++ django-verdant/rca/static/rca/js/site.js
@@ -89,7 +89,7 @@ function showSearchAutocomplete() {
             window.location.href = ui.item.url;
         }
     }).data("ui-autocomplete")._renderItem = function( ul, item ) {
-        return $( "<li></li>" ).data( "item.autocomplete", item ).append( "<a><span>" + item.type + "</span> " + item.label + "</a>" ).appendTo( ul );
+        return $( "<li></li>" ).data( "item.autocomplete", item ).append( "<a>" + item.label + "<span>" + item.type + "</span></a>" ).appendTo( ul );
     };
 }
 
