6391bf2958d682880d55782bedf11af3d2d3c2a2
Dave Cranwell <david@torchbox.com>
2013-11-13 10:43:39 +0000

    ongoing tweaks

--- django-verdant/verdantimages/views/chooser.py
+++ django-verdant/verdantimages/views/chooser.py
@@ -15,7 +15,7 @@ def get_image_json(image):
     helper function: given an image, return the json to pass back to the
     image chooser panel
     """
-    preview_image = image.get_rendition('fill-130x100')
+    preview_image = image.get_rendition('max-130x100')
 
     return json.dumps({
         'id': image.id,
