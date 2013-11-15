94041981f68bee63e86dfd3b41735287852ff66d
Dave Cranwell <david@torchbox.com>
2013-11-15 11:59:07 +0000

    updates to homepage look

--- django-verdant/verdantadmin/views/home.py
+++ django-verdant/verdantadmin/views/home.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
-from core.models import PageRevision
+from core.models import Page, PageRevision
+from verdantimages.models import get_image_model
+from verdantdocs.models import Document
 
 @login_required
 def home(request):
@@ -11,7 +13,15 @@ def home(request):
     else:
         page_revisions_for_moderation = PageRevision.objects.none()
 
+    total_images = get_image_model().objects.count()
+    total_documents = Document.objects.count()
+    total_pages = Page.objects.count()
+
+
     return render(request, "verdantadmin/home.html", {
-    	'site_name': settings.VERDANT_SITE_NAME,
+        'total_pages': total_pages,
+        'total_images': total_images,
+        'total_docs': total_documents,
+        'site_name': settings.VERDANT_SITE_NAME,
         'page_revisions_for_moderation': page_revisions_for_moderation
     })
