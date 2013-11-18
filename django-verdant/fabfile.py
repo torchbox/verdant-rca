557760d5c5219d4aebd1b3fc0690c0a566107a9b
River Tarnell <river@loreley.flyingparchment.org.uk>
2013-11-18 12:12:05 +0000

    Update fabfile for two-server deployment.
    Deployment is now done with two separate tasks: 'deploy' and 'migrate'.
    'deploy' is used to update the git checkout and runs on both servers (rca2 &
    rca3).  'migrate' runs database migrations and restarts celeryd and celerybeat.
    This runs only on rca2, the primary server.
    
    To deploy new code, do the following:
    
    $ fab deploy migrate
    
    If you're sure there are no new migrations, and celeryd/celerybeat don't need
    to be changed, then you just do:
    
    $ fab deploy

--- django-verdant/fabfile.py
+++ django-verdant/fabfile.py
@@ -2,9 +2,11 @@ from __future__ import with_statement
 from fabric.api import *
 
 env.roledefs = {
-    'staging': ['django-staging.torchbox.com'],
-    'production': ['rca2.dh.bytemark.co.uk'],
-    #'production': ['rca2.dh.bytemark.co.uk', 'rca3.dh.bytemark.co.uk'],
+    'staging': ['root@django-staging.torchbox.com'],
+    # This is the host where migrations are done.
+    'production_primary': ['root@rca2.dh.bytemark.co.uk'],
+    # All hosts will be listed here.
+    'production': ['root@rca2.dh.bytemark.co.uk', 'root@rca3.dh.bytemark.co.uk'],
 }
 
 @roles('staging')
@@ -26,18 +28,25 @@ def deploy_staging():
 
 
 @roles('production')
-def deploy_production():
+def deploy():
     with cd('/usr/local/django/verdant-rca/'):
         with settings(sudo_user='verdant-rca'):
             sudo("git pull")
+
+    run("supervisorctl restart verdant-rca")
+
+@roles('production_primary')
+def migrate():
+    with cd('/usr/local/django/verdant-rca/'):
+        with settings(sudo_user='verdant-rca'):
             sudo("/usr/local/django/virtualenvs/verdant-rca/bin/pip install -r django-verdant/requirements.txt")
             sudo("/usr/local/django/virtualenvs/verdant-rca/bin/python django-verdant/manage.py syncdb --settings=verdant.settings.production --noinput")
             sudo("/usr/local/django/virtualenvs/verdant-rca/bin/python django-verdant/manage.py migrate --settings=verdant.settings.production --noinput")
             sudo("/usr/local/django/virtualenvs/verdant-rca/bin/python django-verdant/manage.py collectstatic --settings=verdant.settings.production --noinput")
 
-        sudo("supervisorctl restart verdant-rca")
-        sudo("supervisorctl restart rca-celeryd")
-        sudo("supervisorctl restart rca-celerybeat")
+        run("supervisorctl restart verdant-rca")
+        run("supervisorctl restart rca-celeryd")
+        run("supervisorctl restart rca-celerybeat")
 
         with settings(sudo_user='verdant-rca'):
             sudo("/usr/local/django/virtualenvs/verdant-rca/bin/python django-verdant/manage.py update_index --settings=verdant.settings.production")
