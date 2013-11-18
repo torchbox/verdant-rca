e71b7f2ae0c7a3c40a33aa20159f9b1e5d19a3a5
Karl Hobley <karl@torchbox.com>
2013-11-18 11:12:51 +0000

    Signage: Added query to get events for each day

--- django-verdant/rca_signage/models.py
+++ django-verdant/rca_signage/models.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse, Http404
+from django.db.models import Q
 from django.shortcuts import render
 from core.models import Page
-from rca.models import EventItem
+from rca.models import EventItem, EventItemDatesTimes
 from constants import SCREEN_CHOICES, SCREEN_CHOICES_DICT
 import datetime
 
@@ -23,17 +24,41 @@ class ScreenIndex(Page):
 
     def serve_screen(self, request, screen, extra_path=None):
         # Check for special event
-        special_events = EventItem.objects.filter(screens__screen=screen, special_event=True)
+        special_events = EventItem.objects.filter(screens__screen=screen, special_event=True, live=True)
         if len(special_events) > 0:
             return render(request, 'rca_signage/special_event.html', {
                 'screen': dict(slug = screen, name=SCREEN_CHOICES_DICT[screen]),
                 'event': special_events[0],
             })
         else:
+            # Week start and week end
+            week_start = datetime.date(2013, 9, 28)
+            week_end = datetime.date(2013, 12, 5)
+            day_count = (week_end - week_start).days + 1
+
+            # Get list of dates
+            dates = [
+                week_start + datetime.timedelta(days=day_num)
+                for day_num in range(day_count)
+            ]
+
+            # Add a list of events to each day
+            days = [{
+                'date': date,
+                'events': EventItemDatesTimes.objects.filter(page__live=True).filter(
+                    Q(date_from__lte=date, date_to__gte=date) | # Multi day events
+                    Q(date_from=date, date_to=None) # Single day events
+                )
+            } for date in dates]
+
+            # Remove days without any events
+            days = filter(lambda day: len(day['events']) > 0, days)
+
             return render(request, 'rca_signage/weeks_events.html', {
                 'screen': dict(slug = screen, name=SCREEN_CHOICES_DICT[screen]),
-                'week_start': datetime.date(2013, 11, 28),
-                'week_end': datetime.date(2013, 11, 29),
+                'week_start': week_start,
+                'week_end': week_end,
+                'days': days,
             })
 
         return HttpResponse("This is the screen page '%s' (%s)" % (screen, extra_path))
