bff0209411b997d65d824b907e93470ae5514382
Matt Westcott <matt@west.co.tt>
2013-11-18 15:11:42 +0000

    fix path to /favicon.ico and serve it in dev mode

--- django-verdant/verdant/urls.py
+++ django-verdant/verdant/urls.py
@@ -58,5 +58,10 @@ urlpatterns = patterns('',
 
 if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from django.views.generic.base import RedirectView
+
     urlpatterns += staticfiles_urlpatterns() # tell gunicorn where static files are in dev mode
     urlpatterns += static(settings.MEDIA_URL + 'images/', document_root=os.path.join(settings.MEDIA_ROOT, 'images'))
+    urlpatterns += patterns('',
+        (r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'rca/images/favicon.ico'))
+    )
