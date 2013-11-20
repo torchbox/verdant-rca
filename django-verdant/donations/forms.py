ded905f744cf83fba8bc2afbf4542094fec3f9ef
Dave Cranwell <david@torchbox.com>
2013-11-20 11:46:21 +0000

    donation styling work

--- django-verdant/donations/forms.py
+++ django-verdant/donations/forms.py
@@ -15,6 +15,9 @@ class DonationForm(forms.Form):
         self.fields['name'].widget = forms.HiddenInput()
         self.fields['name'].initial = ""  # name on card is optional and set by javascript
 
+    
+    required_css_class = 'required'
+
     METADATA_FIELDS = ['title', 'first_name', 'last_name', 'is_gift_aid', 'email', 'phone', 'class_year', 'donation_for', 'affiliation', 'not_included_in_supporters_list']  # 'phone_type'
 
     UNREADABLE_FIELDS = ['number', 'cvc', 'expiration']
@@ -27,29 +30,29 @@ class DonationForm(forms.Form):
         ("1000", "£1000"),
         ("", "Other"),
     ))
-    amount = forms.FloatField(required=False)
+    amount = forms.FloatField(required=True)
 
-    number = CreditCardField(label="Card number", required=False)
-    expiration = ExpiryDateField(required=False)
-    cvc = VerificationValueField(required=False, help_text="The 3-digit security code printed (not embossed) on the front of the card, or on the signature strip on the reverse")
+    number = CreditCardField(label="Card number", required=True)
+    expiration = ExpiryDateField(required=True)
+    cvc = VerificationValueField(required=True, help_text="The 3-digit security code printed (not embossed) on the front of the card, or on the signature strip on the reverse")
     is_gift_aid = forms.BooleanField(label="Gift Aid", required=False, help_text="""
         I am eligible as a UK taxpayer and consent to the Royal College of Art
         claiming Gift Aid on my behalf on all qualifying donations
         from the date of this declaration until I notify you otherwise.
     """)
-    email = forms.EmailField(required=False)
+    email = forms.EmailField(required=True)
     not_included_in_supporters_list = forms.BooleanField(label="Please tick this box if you do not wish to be included in our list of supporters", required=False, help_text="")
 
-    title           = forms.CharField(required=False, max_length=255)
-    first_name      = forms.CharField(required=False, max_length=255)
-    last_name       = forms.CharField(required=False, max_length=255)
-    address_line1   = forms.CharField(label="Address line 1", required=False, max_length=255)
+    title           = forms.CharField(required=True, max_length=255)
+    first_name      = forms.CharField(required=True, max_length=255)
+    last_name       = forms.CharField(required=True, max_length=255)
+    address_line1   = forms.CharField(label="Address line 1", required=True, max_length=255)
     address_line2   = forms.CharField(label="Address line 2", required=False, max_length=255)
-    address_city    = forms.CharField(label="Town", required=False, max_length=255)
+    address_city    = forms.CharField(label="Town", required=True, max_length=255)
     address_state   = forms.CharField(label="County", required=False, max_length=255)
-    address_zip     = forms.CharField(label="Postcode", required=False, max_length=255)
-    address_country = forms.CharField(label="Country", required=False, max_length=255)
-    phone           = forms.CharField(required=False, max_length=255)
+    address_zip     = forms.CharField(label="Postcode", required=True, max_length=255)
+    address_country = forms.CharField(label="Country", required=True, max_length=255)
+    phone           = forms.CharField(required=True, max_length=255)
 
     # phone_type = forms.ChoiceField(required=False, choices=(
     #         ("home", "Home"),
@@ -57,14 +60,14 @@ class DonationForm(forms.Form):
     #         ("mobile", "Mobile"),
     # ))
 
-    affiliation = forms.ChoiceField(label="*Affiliation with the RCA", required=False, choices=(
+    affiliation = forms.ChoiceField(label="*Affiliation with the RCA", required=True, choices=(
             ("Alumnus/alumna", "Alumnus/alumna"),
             ("staff", "Staff"),
             ("friend", "Friend"),
             ("parent", "Parent"),
     ))
 
-    donation_for = forms.ChoiceField(label="Please direct my gift towards", required=False, choices=(
+    donation_for = forms.ChoiceField(label="Please direct my gift towards", required=True, choices=(
             ("scholarships", "Scholarships"),
             ("college_greatest_need", "College’s greatest need"),
     ))
