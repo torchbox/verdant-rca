1d3caf71816b35e4f161bd2d7f6756126f754c96
Matt Westcott <matt@west.co.tt>
2013-11-22 15:55:34 +0000

    upgrade to django 1.6

--- django-verdant/verdant/settings/dev.py
+++ django-verdant/verdant/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = True
-INSTALLED_APPS = list(INSTALLED_APPS) + ['devserver']
+# INSTALLED_APPS = list(INSTALLED_APPS) + ['devserver']  # currently broken on Django 1.6 - see https://code.djangoproject.com/ticket/21348
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
