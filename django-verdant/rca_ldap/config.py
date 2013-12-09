72519f20851d3daac82597b403bb0328bb199c4c
Karl Hobley <karl@torchbox.com>
2013-12-09 13:37:35 +0000

    Roles now get pulled in from LDAP

---
+++ django-verdant/rca_ldap/config.py
@@ -0,0 +1,25 @@
+from django_auth_ldap.config import LDAPSearch
+
+
+class LDAPSearchRCA(LDAPSearch):
+    """
+    An extension of the django-auth-ldap.LDAPSearch class to allow user roles to be easily configured
+    """
+
+    def __init__(self, *args, **kwargs):
+        if 'role' in kwargs:
+            self.role = kwargs['role']
+            del kwargs['role']
+        else:
+            self.role = dict(superuser=False, groups=[])
+
+        return super(LDAPSearchRCA, self).__init__(*args, **kwargs)
+
+    def _process_results(self, *args, **kwargs):
+        results = super(LDAPSearchRCA, self)._process_results(*args, **kwargs)
+
+        # Sneak role into results
+        for result in results:
+            result[1]['role'] = self.role
+
+        return results
