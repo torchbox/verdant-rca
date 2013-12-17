2d680aa286dd0f875460af969ee69291cdc49b79
Karl Hobley <karl@torchbox.com>
2013-12-17 10:41:15 +0000

    Embeds: Squashed migrations

--- django-verdant/verdantembeds/migrations/0001_initial.py
+++ django-verdant/verdantembeds/migrations/0001_initial.py
@@ -8,11 +8,11 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'SavedEmbed'
-        db.create_table(u'verdantembeds_savedembed', (
+        # Adding model 'Embed'
+        db.create_table(u'verdantembeds_embed', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('url', self.gf('django.db.models.fields.URLField')(max_length=200)),
-            ('maxwidth', self.gf('django.db.models.fields.SmallIntegerField')(null=True, blank=True)),
+            ('max_width', self.gf('django.db.models.fields.SmallIntegerField')(null=True, blank=True)),
             ('type', self.gf('django.db.models.fields.CharField')(max_length=10)),
             ('html', self.gf('django.db.models.fields.TextField')(blank=True)),
             ('title', self.gf('django.db.models.fields.TextField')(blank=True)),
@@ -21,28 +21,28 @@ class Migration(SchemaMigration):
             ('height', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
             ('last_updated', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
         ))
-        db.send_create_signal(u'verdantembeds', ['SavedEmbed'])
+        db.send_create_signal(u'verdantembeds', ['Embed'])
 
-        # Adding unique constraint on 'SavedEmbed', fields ['url', 'maxwidth']
-        db.create_unique(u'verdantembeds_savedembed', ['url', 'maxwidth'])
+        # Adding unique constraint on 'Embed', fields ['url', 'max_width']
+        db.create_unique(u'verdantembeds_embed', ['url', 'max_width'])
 
 
     def backwards(self, orm):
-        # Removing unique constraint on 'SavedEmbed', fields ['url', 'maxwidth']
-        db.delete_unique(u'verdantembeds_savedembed', ['url', 'maxwidth'])
+        # Removing unique constraint on 'Embed', fields ['url', 'max_width']
+        db.delete_unique(u'verdantembeds_embed', ['url', 'max_width'])
 
-        # Deleting model 'SavedEmbed'
-        db.delete_table(u'verdantembeds_savedembed')
+        # Deleting model 'Embed'
+        db.delete_table(u'verdantembeds_embed')
 
 
     models = {
-        u'verdantembeds.savedembed': {
-            'Meta': {'unique_together': "(('url', 'maxwidth'),)", 'object_name': 'SavedEmbed'},
+        u'verdantembeds.embed': {
+            'Meta': {'unique_together': "(('url', 'max_width'),)", 'object_name': 'Embed'},
             'height': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
             'html': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'last_updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'maxwidth': ('django.db.models.fields.SmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'max_width': ('django.db.models.fields.SmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
             'thumbnail_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
             'title': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'type': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
