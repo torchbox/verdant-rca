251e704b930c1b01c40f98d9b78d340468b62587
Karl Hobley <karl@torchbox.com>
2013-12-18 13:28:18 +0000

    Editors picks views

--- django-verdant/verdantsearch/backend_urls.py
+++ django-verdant/verdantsearch/backend_urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns("verdantsearch.views",
     url(r"^editorspicks/$", "editorspicks.index", name="verdantsearch_editorspicks_index"),
     url(r"^editorspicks/_add/$", "editorspicks.add", name="verdantsearch_editorspicks_add"),
-    url(r"^editorspicks/(?P<slug>[a-z0-9-]+)/$", "editorspicks.edit", name="verdantsearch_editorspicks_edit"),
-    url(r"^editorspicks/(?P<slug>[a-z0-9-]+)/delete/$", "editorspicks.delete", name="verdantsearch_editorspicks_delete"),
+    url(r"^editorspicks/(\d+)/$", "editorspicks.edit", name="verdantsearch_editorspicks_edit"),
+    url(r"^editorspicks/(\d+)/delete/$", "editorspicks.delete", name="verdantsearch_editorspicks_delete"),
 )
--- django-verdant/verdantsearch/views/editorspicks.py
+++ django-verdant/verdantsearch/views/editorspicks.py
@@ -1,24 +1,34 @@
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 from verdantsearch import models
 
 
 @login_required
 def index(request):
     searchterms_list = models.SearchTerms.objects.all()
-    return render(request, 'verdantsearch/editorspicks/index.html', dict(searchterms_list=searchterms_list))
+    return render(request, 'verdantsearch/editorspicks/index.html', {
+        'searchterms_list': searchterms_list
+    })
 
 
 @login_required
 def add(request):
-    pass
+    return render(request, 'verdantsearch/editorspicks/add.html', {
+    })
 
 
 @login_required
 def edit(request, searchterms_id):
-    pass
+    searchterms = get_object_or_404(models.SearchTerms, pk=searchterms_id)
 
+    return render(request, 'verdantsearch/editorspicks/edit.html', {
+        'searchterms': searchterms,
+    })
 
 @login_required
 def delete(request, searchterms_id):
-    pass
+    searchterms = get_object_or_404(models.SearchTerms, pk=searchterms_id)
+
+    return render(request, 'verdantsearch/editorspicks/confirm_delete.html', {
+        'searchterms': searchterms,
+    })
