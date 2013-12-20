135bdaa49f084421ea3b9eff9e67bf555f170987
Karl Hobley <karl@torchbox.com>
2013-12-20 14:54:05 +0000

    Fixed potential URL clash

--- django-verdant/verdantsearch/backend_urls.py
+++ django-verdant/verdantsearch/backend_urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns("verdantsearch.views",
     url(r"^editorspicks/$", "editorspicks.index", name="verdantsearch_editorspicks_index"),
-    url(r"^editorspicks/add/$", "editorspicks.add", name="verdantsearch_editorspicks_add"),
+    url(r"^editorspicks/_add/$", "editorspicks.add", name="verdantsearch_editorspicks_add"),
     url(r"^editorspicks/([a-z0-9-]+)/$", "editorspicks.edit", name="verdantsearch_editorspicks_edit"),
 )
