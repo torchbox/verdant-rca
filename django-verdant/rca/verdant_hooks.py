150c12d1bc5baaadc6b3c38022151f0f6a32090e
Matt Westcott <matt@west.co.tt>
2014-01-14 16:28:23 +0000

    insert RCA Now in left menu in place of Explorer / Search

--- django-verdant/rca/verdant_hooks.py
+++ django-verdant/rca/verdant_hooks.py
@@ -1,14 +1,20 @@
 from verdantadmin import hooks
 from verdantadmin.menu import MenuItem
 
+from django.core.urlresolvers import reverse
+
 def user_is_student(user):
     return [group.name for group in user.groups.all()] == ['Students']
 
 def construct_main_menu(request, menu_items):
     if user_is_student(request.user):
-        pass
-        #menu_items.append(
-        #    MenuItem('Ponies', '/ponies/')
-        #)
+        # remove Explorer and Search items from the menu.
+        # assigning menu_items[:] will modify the list passed in
+        menu_items[:] = [item for item in menu_items if item.name not in ('explorer', 'search')]
+
+        # insert RCA Now item in place
+        menu_items.append(
+            MenuItem('RCA Now', reverse('rca_now_editor_index'), classnames='icon icon-doc-full-inverse', order=100)
+        )
 
 hooks.register('construct_main_menu', construct_main_menu)
