ec6c3504b5a02460fda0ddc2f0bd3ca252077899
Matt Westcott <matt@west.co.tt>
2014-01-14 18:15:01 +0000

    add hooks to redirect students back to the homepage / rca now index after editing

--- django-verdant/rca/verdant_hooks.py
+++ django-verdant/rca/verdant_hooks.py
@@ -1,7 +1,10 @@
 from verdantadmin import hooks
 from verdantadmin.menu import MenuItem
-
 from django.core.urlresolvers import reverse
+from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import redirect
+
+from rca.models import RcaNowPage
 
 def user_is_student(user):
     return [group.name for group in user.groups.all()] == ['Students']
@@ -19,5 +22,16 @@ def construct_main_menu(request, menu_items):
         menu_items.append(
             MenuItem('RCA Now', reverse('rca_now_editor_index'), classnames='icon icon-doc-full-inverse', order=101)
         )
-
 hooks.register('construct_main_menu', construct_main_menu)
+
+def redirect_student_after_edit(request, page):
+    if user_is_student(request.user):
+        # Override the usual redirect to the explorer that happens after adding / deleting / editing a page;
+        # instead, take them to the homepage or the RCA Now index, according to the type of the page just edited.
+        if page.content_type == ContentType.objects.get_for_model(RcaNowPage):
+            return redirect('rca_now_editor_index')
+        else:
+            return redirect('verdantadmin_home')
+hooks.register('after_create_page', redirect_student_after_edit)
+hooks.register('after_edit_page', redirect_student_after_edit)
+hooks.register('after_delete_page', redirect_student_after_edit)
