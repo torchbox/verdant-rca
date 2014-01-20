98b82f49de5e038c564b3019b3571975a9995ec4
Helen C <helen.chapman@torchbox.com>
2014-01-20 10:33:57 +0000

    more stuff on standard page

--- django-verdant/demo/static/demo/js/main.js
+++ django-verdant/demo/static/demo/js/main.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   $('.bxslider').bxSlider({
   	captions: true
   });
+
+  //Apply img-thumbnail class to body-content images
+  $('.body-content img').addClass("img-thumbnail");
 });
