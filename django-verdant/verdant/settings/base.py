fb90c42e734e6fccd38e8c380703b3ced6d4dae2
Matt Westcott <matt@west.co.tt>
2014-01-22 12:27:23 +0000

    Add an initially empty django-wagtail package

--- django-verdant/verdant/settings/base.py
+++ django-verdant/verdant/settings/base.py
@@ -8,6 +8,9 @@ PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '..', '..')
 # Modify sys.path to include the lib directory
 sys.path.append(os.path.join(PROJECT_ROOT, "lib"))
 
+# Add the django-wagtail library dir (which lives alongside PROJECT_ROOT)
+sys.path.append(os.path.join(PROJECT_ROOT, '..', 'django-wagtail'))
+
 # Add dependencies django-cluster and django-treebeard, which live alongside PROJECT_ROOT
 # (until they get released as a standalone project / merged upstream respectively)
 sys.path.append(os.path.join(PROJECT_ROOT, '..', 'django-cluster'))
