eebd19c8cc6daabb9ce0288b368416334f010ce3
Karl Hobley <karl@torchbox.com>
2014-01-27 13:14:01 +0000

    Filters code is now more generic

--- django-verdant/rca/filters.py
+++ django-verdant/rca/filters.py
@@ -1,49 +1,35 @@
 import json
 
 
+def run_filters(queryset, filters):
+    filters_out = []
+
+    # Iterate through filters
+    for name, field, current_value in filters:
+        # Get options
+        options = [
+            values[field]
+            for values in queryset.order_by(field).distinct(field).values(field)
+        ]
+
+        # Apply filter to queryset
+        if current_value:
+            queryset_filtered = queryset.filter(**{field: current_value})
+
+            # Only apply the filter if there are values
+            if queryset_filtered.exists():
+                queryset = queryset_filtered
+            else:
+                current_value = None
+
+        # Add filter to output
+        filters_out.append(dict(name=name, current_value=current_value, options=options))
+
+    return queryset, filters_out
+
+
 def run_school_programme_filters(queryset, school, programme, school_field='school', programme_field='programme'):
-    # Get list of related schools
-    related_schools = [
-        values[school_field]
-        for values in queryset.order_by(school_field).distinct(school_field).values(school_field)
-    ]
-
-    # Apply school filter
-    if school:
-        # See below comment for explanation
-        queryset_school = queryset.filter(**{school_field: school})
-
-        if queryset_school.exists():
-            queryset = queryset_school
-        else:
-            school = None
-
-    # Get list of related programmes
-    related_programmes = [
-        values[programme_field]
-        for values in queryset.order_by(programme_field).distinct(programme_field).values(programme_field)
-    ]
-
-    # Apply programme filter
-    if programme:
-        # Only apply programme filter if there are items this programme
-        # Reason: If a programme is chosen, and the user changes to a different school,
-        # there may be no results displayed (as the programme might not exist in the new school).
-        # This check automatically unsets the programme in this case.
-        queryset_programme = queryset.filter(**{programme_field: programme})
-
-        if queryset_programme.exists():
-            queryset = queryset_programme
-        else:
-            programme = None
-
-    # This dictionay needs to become a javascript variable called "filters_context".
-    # It gets used inside the filters.js file
-    filters_context = json.dumps({
-        'selected_school': school if school else '',
-        'selected_programme': programme if programme else '',
-        'related_schools': related_schools,
-        'related_programmes': related_programmes,
-    })
-
-    return queryset, filters_context
+    return run_filters(queryset, [
+        ('school', school_field, school),
+        ('programme', programme_field, programme),
+    ])
