7b0297971b9b3eef79c653fcb55aa50d99cd8e08
Karl Hobley <karl@torchbox.com>
2014-03-03 17:26:24 +0000

    Revert "Revert "Revert "Revert "Revert "Merge branch 'feature/showrca2014' into develop"""""
    This reverts commit c9a109778436a99812e6ef339e18f54c130be362.

--- django-verdant/rcasite/settings/dev.py
+++ django-verdant/rcasite/settings/dev.py
@@ -8,13 +8,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # BASE_URL required for notification emails
 BASE_URL = 'http://localhost:8111'
 
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
-
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
 
 try:
     from .local import *
