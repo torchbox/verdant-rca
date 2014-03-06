d245a48bfc7ba10fcdf12fba96967531cf31e972
Serafeim Papastefanos <spapas@gmail.com>
2014-03-07 00:16:27 +0200

    Fix to show staff role for staff_feed.html
    Copied the staff_role to the staff page in models.py (since it was in the
    parent model) and changed {{ staff }} to {{ staff.staff_role }} to
    staff_feed.html

--- django-verdant/rca/models.py
+++ django-verdant/rca/models.py
@@ -850,10 +850,14 @@ class ProgrammePage(Page, SocialFields):
         # Get staff from manual feed
         feed = self.manual_staff_feed.all()
 
-        # Get each staffpage out of the feed
-        feed = [staffpage.staff for staffpage in feed]
+        # Get each staffpage out of the feed and add their role
+        feed2 = []
+        for staffpage in feed:
+            staff = staffpage.staff
+            staff.staff_role = staffpage.staff_role
+            feed2.append(staff)
 
-        return feed
+        return feed2
 
     @vary_on_headers('X-Requested-With')
     def serve(self, request):
