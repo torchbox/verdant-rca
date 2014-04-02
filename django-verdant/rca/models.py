46e86301822dab8c76fd8f323e82bbb70f7328e5
Karl Hobley <karl@torchbox.com>
2014-04-02 09:15:48 +0100

    Show page now defaults to 2013 students

--- django-verdant/rca/models.py
+++ django-verdant/rca/models.py
@@ -4407,7 +4407,7 @@ class GalleryPage(Page, SocialFields):
         # Get filter parameters
         school = request.GET.get('school')
         programme = request.GET.get('programme')
-        year = request.GET.get('degree_year')
+        year = request.GET.get('degree_year') or '2013'
 
         # Get students
         ma_students_q, mphil_students_q, phd_students_q, filters = self.get_students_q(school, programme, year)
