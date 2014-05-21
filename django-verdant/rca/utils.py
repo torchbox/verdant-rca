9f91b8bdedcb397e257a5b0724458965f4f8f359
Karl Hobley <karl@torchbox.com>
2014-05-21 12:09:52 +0100

    Fixed issue with get_programmes not returning None. Documented functions in rca/utils.py

--- django-verdant/rca/utils.py
+++ django-verdant/rca/utils.py
@@ -1,20 +1,38 @@
 from .models import SCHOOL_PROGRAMME_MAP
 
+
 def get_school_programme_map(year=None):
+    """
+    This function gets a mapping of schools to programmes for a particular year
+
+    If the year is unspecified, then the latest year in the SCHOOL_PROGRAMME_MAP will be used
+    """
     if year:
-        return SCHOOL_PROGRAMME_MAP[year]
+        if year in SCHOOL_PROGRAMME_MAP:
+            return SCHOOL_PROGRAMME_MAP[year]
+        else:
+            return
     else:
         return SCHOOL_PROGRAMME_MAP[max(SCHOOL_PROGRAMME_MAP.keys())]
 
 
 def get_programmes(school=None, year=None):
+    """
+    This function gets a list of programme slugs for the specified school/year
+
+    If the school is unspecified, all programmes for the year will be returned
+    If year is unspecified, the latest year in the SCHOOL_PROGRAMME_MAP will be used
+    If the school or year does not exist, this function will return None
+    """
     school_programme_map = get_school_programme_map(year)
+    if not school_programme_map:
+        return
 
     if school:
         if school in school_programme_map:
             return school_programme_map[school]
         else:
-            return []
+            return
     else:
         programmes = []
         for extra_programmes in school_programme_map.values():
@@ -23,7 +41,15 @@ def get_programmes(school=None, year=None):
 
 
 def get_school_for_programme(programme, year=None):
+    """
+    This function takes a programme slug and returns the school slug that the programme is in
+
+    If year is unspecified, the latest year in the SCHOOL_PROGRAMME_MAP will be used
+    If the programme or year does not exist, this function will return None
+    """
     school_programme_map = get_school_programme_map(year)
+    if not school_programme_map:
+        return
 
     for school, programmes in school_programme_map.items():
         if programme in programmes:
