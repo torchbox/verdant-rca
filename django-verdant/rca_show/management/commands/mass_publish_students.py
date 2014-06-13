666f362cb045be8339c608985d6fbc803e6add76
Karl Hobley <karl@torchbox.com>
2014-06-13 12:34:52 +0100

    Added script to mass publish student pages

---
+++ django-verdant/rca_show/management/commands/mass_publish_students.py
@@ -0,0 +1,52 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from rca.models import NewStudentPage
+
+
+class Command(BaseCommand):
+    def find_student_page(self, student):
+        # Student info
+        programme = student[0]
+        first_name = student[2]
+        last_name = student[1]
+        email = student[3]
+
+        print first_name, last_name
+
+        # Get list of possible pages
+        students = NewStudentPage.objects.all()
+
+        # Find student page
+        page = None
+
+        # Find by email
+        if page is None and email:
+            page = students.filter(emails__email=email).first()
+
+        # Find by owner email
+        if page is None and email:
+            page = students.filter(owner__email=email).first()
+
+        # Find by name
+        if page is None:
+            page = students.filter(last_name__iexact=last_name, first_name__iexact=first_name).first()
+
+        return page
+
+    def handle(self, filename, **options):
+        with open(filename) as f:
+            # Get list of students
+            students = (self.find_student_page(student) for student in csv.reader(f))
+
+            # Publish them
+            for student in students:
+                if student:
+                    if student.has_unpublished_changes or not student.live:
+                        student.get_latest_revision().publish()
+                        print "PUBLISHED:", student.id
+                    else:
+                        print "ALREADY LIVE:", student.id
+                else:
+                    print "CANNOT FIND PAGE"
