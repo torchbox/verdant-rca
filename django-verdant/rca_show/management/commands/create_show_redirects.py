fe3649443cdacb1cc18e1b92a3b12b663eb07d89
Karl Hobley <karl@torchbox.com>
2014-06-16 13:03:02 +0100

    Create redirects from students name instead of title

--- django-verdant/rca_show/management/commands/create_show_redirects.py
+++ django-verdant/rca_show/management/commands/create_show_redirects.py
@@ -13,7 +13,7 @@ class Command(BaseCommand):
 
         for student in show_index.get_students():
             # Work out a URL to redirect from
-            from_url = 'show2014/' + slugify(student.title) + '/'
+            from_url = 'show2014/' + slugify(student.first_name) + '_' + slugify(student.last_name) + '/'
 
             # Find students url inside 
             if show_index.is_programme_page:
