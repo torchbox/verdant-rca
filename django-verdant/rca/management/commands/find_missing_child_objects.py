d2dc36ce1e0f532fe9bfc23103e962769af13d11
Karl Hobley <karl@torchbox.com>
2014-06-17 10:21:27 +0100

    Minor cleanup of find_missing_child_objects script

--- django-verdant/rca/management/commands/find_missing_child_objects.py
+++ django-verdant/rca/management/commands/find_missing_child_objects.py
@@ -1,11 +1,15 @@
-from django.core.management.base import NoArgsCommand
-from wagtail.wagtailcore.models import get_page_types
 import json
+
+from django.core.management.base import NoArgsCommand
+
+from wagtail.wagtailcore.models import get_page_types
+
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         for content_type in get_page_types():
             print "scanning %s" % content_type.name
+
             page_class = content_type.model_class()
 
             if not hasattr(page_class._meta, 'child_relations'):
@@ -16,9 +20,8 @@ class Command(NoArgsCommand):
                 relations_by_name[rel.get_accessor_name()] = rel
 
             for page in page_class.objects.all():
-                try:
-                    revision = page.revisions.order_by('-created_at')[0]
-                except IndexError:
+                revision = page.revisions.order_by('-created_at').first()
+                if not revision:
                     continue
 
                 revision_data = json.loads(revision.content_json)
