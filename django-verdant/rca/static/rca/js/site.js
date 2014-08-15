e7fce5ac6115f5852c9fbf0cbe50d9c931154867
Chris Rogers <chris.rogers@host-2-41.dhc.internal.torchbox.com>
2014-08-15 10:48:26 +0100

    beginning show/hide body

--- django-verdant/rca/static/rca/js/site.js
+++ django-verdant/rca/static/rca/js/site.js
@@ -184,8 +184,16 @@ $(function(){
     showHideSlide('.profile .showPractice', '.profile .practice', '.profile .practice');
     showHideSlide('.profile .showExternalCollaborations', '.profile .external-collaborations', '.profile .external-collaborations');
     showHideSlide('.profile .showPublications', '.profile .publications', '.profile .publications');
+    showHideSlide('.index-intro .showBody', '.body-text', '.body-text');
     /* change text on show more button to 'hide' once it has been clicked */
     $('.profile .showmore').click(function(eventObject){
+        if($(this).html() == 'hide'){
+            $(this).html('show more');
+        } else {
+            $(this).html('hide');
+        }
+    });
+    $('.index-intro .showmore').click(function(eventObject){
         if($(this).html() == 'hide'){
             $(this).html('show more');
         } else {
