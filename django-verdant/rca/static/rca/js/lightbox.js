77009e98432dc39a9436603306c146e23e63cfbd
Dave Cranwell <david@torchbox.com>
2014-09-25 11:07:47 +0100

    tweaks to lightbox dimensions

--- django-verdant/rca/static/rca/js/lightbox.js
+++ django-verdant/rca/static/rca/js/lightbox.js
@@ -10,7 +10,14 @@ $(function(){
             overlayClose: true,
             opacity: 0.7,
         	inline: function(){ return $(this).hasClass('inline')},
-        	width: function(){ return ($(this).hasClass('inline')) ? "50%" : null }
+        	width: function(){ return ($(this).hasClass('inline')) ? "50%" : null },
+            height: function(){ return ($(this).hasClass('inline')) ? null : ($(window).height() * (0.9)) },
+            onOpen: function(){
+                $('body').addClass('gallery-lightbox-open');
+            },
+            onClosed: function(){
+                $('body').removeClass('gallery-lightbox-open');
+            }
         });
 
         // then run the item clicked
