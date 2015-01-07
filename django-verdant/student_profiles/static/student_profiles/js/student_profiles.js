a2d55893aa5356495621c65689019c2a83b8517d
Johannes Spielmann <johannes.spielmann@torchbox.com>
2015-01-07 10:16:53 +0100

    javascript cleanup: fixing change event being triggered during initialization

--- django-verdant/student_profiles/static/student_profiles/js/student_profiles.js
+++ django-verdant/student_profiles/static/student_profiles/js/student_profiles.js
@@ -90,6 +90,11 @@ $(document).bind('dragover', function (e)
 */ 
 function activateImageUpload(for_id, options) {
 
+    // prevent the automatic drop handler because we want specific drop zones
+    $(document).bind('drop dragover', function (e) {
+        e.preventDefault();
+    });
+
     // remember the original add function for later because we're going to overwrite it
     var originalAdd = $.blueimp.fileupload.prototype.options.add;
     var containerElement = $($('#' + for_id));
@@ -263,75 +268,6 @@ $('form.student-profile input[type="submit"]').click(function() {
     window.onbeforeunload = null;
     stopAutosave();
 });
-
-
-
-/**
- *  Enable auto-saving for the given form.
- */
-var autosaveTimers = {}
-function enableAutosave(form) {
-    autosaveTimers[form] = 0;
-    var delay = 4000;
-    var delayShowSaved = 2000;
-    var saveString = '';
-    var overlay = $('<div id="overlay">Saving...</div>').appendTo(document.body).css('top', '-3em');
-    
-    function save(element) {
-        clearTimeout(autosaveTimers[form]);   // we clear the timeout because we only want to save n seconds after the last edit
-        window.onbeforeunload = confirmOnPageExit;
-
-        autosaveTimers[form] = setTimeout(function() {
-            window.onbeforeunload = null;
-            var dataString = form.serialize();
-            if (saveString == dataString)
-            {
-                // no need to save because nothing changed
-                return;
-            }
-            saveString = dataString;
-            
-            overlay.html('Saving...').animate({'top': 0}, 400);
-            
-            $.ajax({
-                type: "POST",
-                data: dataString,
-                success: function(msg) {
-                    if (msg.ok)
-                    {
-                        $('#overlay').html('Saved!');
-                        setTimeout(function() {
-                            overlay.animate({'top': '-3em'}, 400);
-                        }, 2000);
-                    }
-                    else {
-                        overlay.css('background-color', '#f00').html('Could not save. Please save manually!');
-                    }
-                },
-                error: function(msg) {
-                    overlay.css('background-color', '#f00').html('Could not save. Please save manually!');
-                }
-            });
-        }, delay);
-    }
-    
-    $(form).find('input').each(function(i, val) {
-        $(val).change(save);
-        $(val).keyup(save);
-    });
-    $(form).find('select').each(function(i, val) {
-        $(val).change(save);
-    });
-}
-// stop any autosave timers that might be running at the moment
-function stopAutosave() {
-    for (i in autosaveTimers)
-    {
-        clearTimeout(autosaveTimers[i]);
-    }
-}
-// and then immediately enable it for the profile-form in the view
-enableAutosave($('form.student-profile'));
 
 
 /*
