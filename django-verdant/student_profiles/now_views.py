e0f059aaad542669331bf8155241c18e488a591d
Johannes Spielmann <johannes.spielmann@torchbox.com>
2015-01-07 21:29:46 +0100

    overview, preview and submission for now-pages

--- django-verdant/student_profiles/now_views.py
+++ django-verdant/student_profiles/now_views.py
@@ -21,6 +21,19 @@ from .forms import ImageForm
 # MAKE SURE IT IS CORRECT FOR YOUR INSTANCE!
 RCA_NOW_INDEX_ID = 36
 
+################################################################################
+## helper functions
+
+def get_page_or_404(request, page_id):
+    """
+    Get the page with the given id if it belongs to the user in the request, otherwise throw a 404.
+    """
+    index_page = Page.objects.get(id=RCA_NOW_INDEX_ID)
+    return get_object_or_404(index_page.get_children(), owner=request.user, id=page_id).get_latest_revision_as_page()
+
+
+################################################################################
+## view functions
 
 @login_required
 def overview(request):
@@ -29,7 +42,8 @@ def overview(request):
     """
     data = {}
 
-    raw_pages = RcaNowPage.objects.filter(owner=request.user, parent_id=RCA_NOW_INDEX_ID)
+    index_page = Page.objects.get(id=RCA_NOW_INDEX_ID)
+    raw_pages = index_page.get_children().filter(owner=request.user)
     data['pages'] = [p.get_latest_revision_as_page() for p in raw_pages]
     for p in data['pages']:
         p.waiting_for_moderation = p.revisions.filter(submitted_for_moderation=True).exists()
@@ -49,10 +63,20 @@ def edit(request, page_id):
 
 @login_required
 def preview(request, page_id):
-    pass
+    page = get_page_or_404(request, page_id)
+    return page.serve(page.dummy_request())
 
 
 @require_POST
 @login_required
 def submit(request, page_id):
-    pass
+    page = get_page_or_404(request, page_id)
+
+    page.save_revision(
+        user=request.user,
+        submitted_for_moderation=True,
+    )
+
+    messages.success(request, "Blog page '{}' was submitted for moderation".format(page.title))
+
+    return redirect('nowpages:overview')
