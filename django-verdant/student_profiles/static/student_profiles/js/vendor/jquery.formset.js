fd7d2dfc194149b5b72d17a0b4043221049c7af3
James Morris <jamesm@torchbox.com>
2015-01-12 17:18:29 +0000

    Basic styling and template layout

--- django-verdant/student_profiles/static/student_profiles/js/vendor/jquery.formset.js
+++ django-verdant/student_profiles/static/student_profiles/js/vendor/jquery.formset.js
@@ -195,9 +195,9 @@
     $.fn.formset.defaults = {
         prefix: 'form',                  // The form prefix for your django formset
         formTemplate: null,              // The jQuery selection cloned to generate new form instances
-        addText: 'add another',          // Text for the add link
-        deleteText: 'remove',            // Text for the delete link
-        addCssClass: 'add-row',          // CSS class applied to the add link
+        addText: 'Add another',          // Text for the add link
+        deleteText: 'Remove',            // Text for the delete link
+        addCssClass: 'add-row button button-small',          // CSS class applied to the add link
         deleteCssClass: 'delete-row',    // CSS class applied to the delete link
         formCssClass: 'dynamic-form',    // CSS class applied to each form in a formset
         extraClasses: [],                // Additional CSS classes, which will be applied to each form in turn
