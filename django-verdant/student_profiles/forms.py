f5a083d884656cd81313c166abe92839bb15a54f
Johannes Spielmann <johannes.spielmann@torchbox.com>
2015-02-01 21:56:20 +0100

    showing upload bar all the time with better explanatory text

--- django-verdant/student_profiles/forms.py
+++ django-verdant/student_profiles/forms.py
@@ -110,7 +110,7 @@ class ImageInput(forms.FileInput):
                     </div> 
                     <i class="icon clearbutton action ion-android-delete" {hidden_clear} title="Remove"></i>
                 </div>
-                <div class="dropzone">Drop file here</div>
+                <div class="dropzone">Drop file here<br>to set image</div>
                 <input type="hidden" id="id_{name}_val" name="{name}_val" value="{value_id}">
 
             </div>""".format(
