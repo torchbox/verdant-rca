16fad61d3fdf7769bf466abef60a005e88b77628
Johannes Spielmann <johannes.spielmann@torchbox.com>
2015-02-01 21:58:30 +0100

    changing error message for autosave to be less scary

--- django-verdant/student_profiles/static/student_profiles/js/autosave.js
+++ django-verdant/student_profiles/static/student_profiles/js/autosave.js
@@ -38,11 +38,11 @@ function enableAutosave(formsel) {
                         }, 2000);
                     }
                     else {
-                        overlay.css('background-color', '#f00').html('Could not save. Please save manually!');
+                        overlay.css('background-color', '#f00').html('There might messages for this form. Please save manually!');
                     }
                 },
                 error: function(msg) {
-                    overlay.css('background-color', '#f00').html('Could not save. Please save manually!');
+                    overlay.css('background-color', '#f00').html('There might be messages for this form. Please save manually!');
                 }
             });
         }, delay);
