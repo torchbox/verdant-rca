fb804347ecae1604160738712e2bcedc843657b5
Dave Cranwell <david@torchbox.com>
2015-04-20 14:47:02 +0100

    making 2014 styles generic

--- django-verdant/rca_show/static/rca_show/js/site.js
+++ django-verdant/rca_show/static/rca_show/js/site.js
@@ -70,40 +70,17 @@ function displayShowOverlay(){
 }
 
 $(function(){
-    Harvey.attach(breakpoints.mobile, {
-        setup: function(){
-          
-        },
-        on: function(){
-            
-        },
-        off: function(){
-          
-        }
-    });
-
-    Harvey.attach(breakpoints.desktopSmall, {
-        setup: function(){
-
-        },
-        on: function(){
-          
-        },
-        off: function(){
-            
-        }
-    });
-
-    overlay = $('#showrca2014-overlay');
+    overlay = $('#overlay');
 
     if(overlay.length){
+        var cookieName = '2015';
+        
         randTheme();
 
-        // if(window.debug || !$.cookie('showrca2014')){
-        if(!$('body').hasClass('type-login') && !$.cookie('showrca2014')){
+        if(window.debug || (!$('body').hasClass('type-login') && !$.cookie(cookieName))){
             setupOverlay();
             displayShowOverlay();
-            $.cookie('showrca2014', '1', { expires: 0.04, path: '/' });
+            $.cookie(cookieName, '1', { expires: 0.04, path: '/' });
         }
         
         $('.toggleoverlay').click(function(){
