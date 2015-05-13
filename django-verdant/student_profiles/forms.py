57c0fc6faa4b6555682c82c07fe606485479c1e8
Karl Hobley <karlhobley10@gmail.com>
2015-05-13 18:09:11 +0100

    Revert "fix: fix typo"
    This reverts commit d69fb130bb043bd5f4d6052cebcc3ebf6c12a8c7.

--- django-verdant/student_profiles/forms.py
+++ django-verdant/student_profiles/forms.py
@@ -177,7 +177,7 @@ class ImageForm(forms.Form):
         self.min_dim = None
         if 'max_size' in kwargs:
             self.max_size = kwargs.pop('max_size')
-        else:
+        elif:
             self.max_size = 1024 * 1024
         if 'min_dim' in kwargs:
             self.min_dim = kwargs.pop('min_dim')
