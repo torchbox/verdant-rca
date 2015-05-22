550d0eea0af2edbca00163113b13f1a764e4ca30
Johannes Spielmann <johannes.spielmann@torchbox.com>
2015-05-22 17:16:12 +0200

    #710: making submission warning more aggressive because "I dont remember any pop ups"

--- django-verdant/student_profiles/static/student_profiles/js/student_profiles.js
+++ django-verdant/student_profiles/static/student_profiles/js/student_profiles.js
@@ -568,7 +568,7 @@ stickyNote();
 * Catch Save and Submit
 */
 $('.submit-page').click(function(e) {
-    if (window.confirm("DO NOT CLICK YES UNLESS YOUR PROFILE IS COMPLETE!\n\nSending this form for moderation means you can no longer make changes or add content to your profile. Please check you have completed all sections before clicking OK.")) {
+    if (window.confirm("DO NOT CLICK YES UNLESS YOUR PROFILE IS COMPLETE!\n\nYOU WILL NOT BE ABLE TO EDIT ANYTHING AFTER CLICKING OK, INCLUDING POSTCARD IMAGES!\n\nSending this form for moderation means you can no longer make changes or add content to your profile. Please check you have completed all sections before clicking OK.")) {
         $('#js_submit_for_moderation').val('submit_for_moderation');
         $(this).parents('form').submit();
         return true;
