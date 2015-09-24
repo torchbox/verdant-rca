24e07e4bdd238540fb56b15669912fbac053c8d3
Karl Hobley <karlhobley10@gmail.com>
2015-09-24 13:07:34 +0100

    redis_cache => django_redis

--- django-verdant/rcasite/settings/base.py
+++ django-verdant/rcasite/settings/base.py
@@ -235,11 +235,11 @@ LOGGING = {
 
 CACHES = {
     'default': {
-        'BACKEND': 'redis_cache.cache.RedisCache',
+        'BACKEND': 'django_redis.cache.RedisCache',
         'LOCATION': '127.0.0.1:6379:1',
         'KEY_PREFIX': 'rca',
         'OPTIONS': {
-            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     }
 }
