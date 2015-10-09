7144593f88c3f9d7cabb81129654817fb335313d
Balazs <balazs.endresz@gmail.com>
2015-10-09 09:29:25 +0100

    Changed the name of the Printmaking programme to only Print

--- django-verdant/rca/models.py
+++ django-verdant/rca/models.py
@@ -302,7 +302,7 @@ ALL_PROGRAMMES = tuple(sorted([
     ('innovationdesignengineering', 'Innovation Design Engineering'),
     ('historyofdesign', 'History of Design'),
     ('fashionmenswear', 'Fashion Menswear'),
-    ('printmaking', 'Printmaking'),
+    ('printmaking', 'Print'),
     ('globalinnovationdesign', 'Global Innovation Design'),
     ('architecture', 'Architecture'),
     ('interiordesign', 'Interior Design'),
