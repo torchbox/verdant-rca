ce52c16c199afcb5dead0a6b3d2ef460eef7c9bf
Balazs <balazs.endresz@gmail.com>
2015-11-16 13:02:49 +0000

    Added merge migration and removed a duplicate one (for programme choices) that's been already added from another branch

---
+++ django-verdant/rca/migrations/0018_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rca', '0016_auto_20151106_0938'),
+        ('rca', '0017_auto_20151106_1550'),
+    ]
+
+    operations = [
+    ]
