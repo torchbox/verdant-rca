5d8dbf463c193c1d580007e183f8462b532a8407
Balazs <balazs.endresz@gmail.com>
2015-12-01 18:06:41 +0000

    Enabled TEMPLATE_DEBUG in dev settings

--- django-verdant/rcasite/settings/dev.py
+++ django-verdant/rcasite/settings/dev.py
@@ -1,6 +1,8 @@
 from .base import *
 
 DEBUG = True
+TEMPLATE_DEBUG = True
+
 # INSTALLED_APPS = list(INSTALLED_APPS) + ['devserver']  # currently broken on Django 1.6 - see https://code.djangoproject.com/ticket/21348
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
