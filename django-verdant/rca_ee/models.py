f1fedda7fd99193d1005adefaee3653fdf18534c
Johannes Spielmann <johannes.spielmann@torchbox.com>
2016-04-13 12:18:33 +0200

    reverting course upload field and document storage

--- django-verdant/rca_ee/models.py
+++ django-verdant/rca_ee/models.py
@@ -1,6 +1,3 @@
-import json
-
-from django.core.exceptions import ValidationError
 from django.shortcuts import render
 from django.db import models
 from modelcluster.fields import ParentalKey
@@ -57,20 +54,6 @@ CourseDocument.content_panels = [
 ]
 
 
-class CourseNeedsUpload(models.Model):
-    """Define that a course needs a portfolio upload before you can register interest."""
-
-    page = ParentalKey('FormPage', related_name='upload_necessary')
-
-    for_course = models.TextField(
-        help_text="The name of the course for which a portfolio upload is necessary before registering interest. Please enter the name of the course exactly as it appears in the selection form below.",
-    )
-
-    content_panels = [
-        FieldPanel('for_course'),
-    ]
-
-
 class FormPage(ExtendedAbstractEmailForm):
     """
     A page for registering interest in a selection of courses.
@@ -79,60 +62,24 @@ class FormPage(ExtendedAbstractEmailForm):
     thank_you_text = RichTextField(blank=True)
 
     def serve(self, request):
-        context = self.get_context(request)
-        context['upload_vals'] = json.dumps([
-            d.for_course.lower() for d in self.upload_necessary.all()
-        ])
-
         if request.method == 'POST':
             form = self.get_form(request.POST)
 
-            try:
-                if form.is_valid():
-                    portfolio_necessary_courses = set([
-                        d.for_course.lower().strip() for d in self.upload_necessary.all()
-                    ])
-                    context['selected_courses'] = selected_courses = set(
-                        [c.lower().strip() for c in form.cleaned_data.get('course', ())]
-                        + [c.lower().strip() for c in form.cleaned_data.get('masterclasses', ())]
-                        + [c.lower().strip() for c in form.cleaned_data.get('residencies', ())]
-                        + [c.lower().strip() for c in form.cleaned_data.get('workshops', ())]
-                    )
+            if form.is_valid():
+                self.process_form_submission(form)
 
-                    if selected_courses & portfolio_necessary_courses:
-                        # a portfolio upload is necessary
-                        # check if a portfolio was uploaded and if not, raise a ValidationError
-                        if "portfolio" not in request.FILES:
-                            context['portfolio_upload_error'] = True
-                            raise ValidationError('Portfolio upload is necessary')
-                        else:
-                            # store the portfolio file as a document
-                            from wagtail.wagtaildocs.models import Document
-                            d = Document(
-                                uploaded_by_user=None,
-                                title='Portfolio upload for {} {}'.format(
-                                    form.cleaned_data['first-name'],
-                                    form.cleaned_data['last-name']
-                                ),
-                                file=request.FILES['portfolio'],
-                            )
-                            d.save()
-                            form.cleaned_data['portfolio_document_id'] = d.id
-
-                    self.process_form_submission(form)
-
-                    context['form'] = form
-                    # render the landing_page
-                    return render(
-                        request,
-                        self.landing_page_template,
-                        context,
-                    )
-            except ValidationError:
-                pass
+                context = self.get_context(request)
+                context['form'] = form
+                # render the landing_page
+                return render(
+                    request,
+                    self.landing_page_template,
+                    context,
+                )
         else:
             form = self.get_form()
 
+        context = self.get_context(request)
         context['form'] = form
         return render(
             request,
@@ -145,7 +92,6 @@ FormPage.content_panels = [
     FieldPanel('title', classname="full title"),
     FieldPanel('intro', classname="full"),
     FieldPanel('thank_you_text', classname="full"),
-    InlinePanel('upload_necessary', label='Needs portfolio update'),
     InlinePanel('documents', label='Documents'),
     InlinePanel('form_fields', label="Form fields"),
 ]
