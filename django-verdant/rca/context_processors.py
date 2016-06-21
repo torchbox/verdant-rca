4ba59618377fee734d2f81b6f76e6276b193e0e2
Karl Hobley <karl@torchbox.com>
2016-06-21 16:47:36 +0100

    Updated global_vars context processor to use taxonomy models

--- django-verdant/rca/context_processors.py
+++ django-verdant/rca/context_processors.py
@@ -1,4 +1,7 @@
 from django.conf import settings
+from django.db.models import Max
+
+from taxonomy.models import Programme, School, Area
 
 from rca.models import YEARS, SCHOOL_PROGRAMME_MAP, SCHOOL_CHOICES, ALL_PROGRAMMES, PROGRAMME_CHOICES, EVENT_LOCATION_CHOICES, AREA_CHOICES, EVENT_AUDIENCE_CHOICES, RESEARCH_TYPES_CHOICES, WORK_THEME_CHOICES, WORK_TYPES_CHOICES, STAFF_TYPES_CHOICES, INNOVATIONRCA_PROJECT_TYPES_CHOICES, SHOW_SCHOOLS, SUSTAINRCA_CATEGORY_CHOICES
 from reachout_choices import REACHOUT_PROJECT_CHOICES, REACHOUT_PARTICIPANTS_CHOICES, REACHOUT_THEMES_CHOICES, REACHOUT_PARTNERSHIPS_CHOICES
@@ -6,30 +9,21 @@ from datetime import date
 
 
 def global_vars(request):
-    year = date.today().year
+    all_areas = Area.objects.all()
+    all_schools = School.objects.all()
+    all_programmes = Programme.objects.all()
+    current_year = 2016  # FIXME
 
-    try:
-        schools_current_year = SCHOOL_PROGRAMME_MAP[str(year)].keys()
-    except KeyError:
-        # If there's no academic year specified for the current year then choose the last one.
-        year = int(sorted(SCHOOL_PROGRAMME_MAP.keys())[-1])
-        schools_current_year = SCHOOL_PROGRAMME_MAP[str(year)].keys()
-
-    schools_current_year = filter(lambda s: s[0] in schools_current_year, SCHOOL_CHOICES)
-
-    years_until_current_year = [y for y in YEARS if int(y) <= year]
-
-    academic_year = '%s/%s' % (year - 1, str(year)[2:])  # 2015 -> 2014/15
-    global_programmes = dict(PROGRAMME_CHOICES)[academic_year]
+    years_until_current_year = [y for y in YEARS if int(y) <= current_year]
 
     return {
-        'global_all_schools': SCHOOL_CHOICES,
-        'global_schools': schools_current_year,
+        'global_all_schools': all_schools.values_list('slug', 'display_name'),
+        'global_schools': all_schools.values_list('slug', 'display_name'),
         'global_show_schools': SHOW_SCHOOLS,
-        'global_all_programmes': ALL_PROGRAMMES,
-        'global_programmes': global_programmes,
+        'global_all_programmes': all_programmes.values_list('slug', 'display_name'),
+        'global_programmes': all_programmes.values_list('slug', 'display_name'),
         'global_locations': EVENT_LOCATION_CHOICES,
-        'global_areas': AREA_CHOICES,
+        'global_areas': all_areas.values_list('slug', 'display_name'),
         'global_years': years_until_current_year,
         'global_audiences': EVENT_AUDIENCE_CHOICES,
         'global_research_types': RESEARCH_TYPES_CHOICES,
