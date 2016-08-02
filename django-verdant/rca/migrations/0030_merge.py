881c928baa2d09f3ae0559c3585ccebb48d6769d
Karl Hobley <karl@torchbox.com>
2016-08-02 12:24:03 +0100

    Merge migration for student and staff taxonomy migrations

---
+++ django-verdant/rca/migrations/0030_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rca', '0029_migrate_student_revisions'),
+        ('rca', '0029_auto_20160705_1240'),
+    ]
+
+    operations = [
+    ]
