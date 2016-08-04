4916cafbe1cb5711a7f0665eba99baf9a013febf
Karl Hobley <karl@torchbox.com>
2016-08-04 11:01:58 +0100

    Created merge migration

---
+++ django-verdant/rca/migrations/0031_merge.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rca', '0030_taxonomy_programme_page_remove_old_fields'),
+        ('rca', '0030_merge'),
+        ('rca', '0027_toxonomy_press_release_remove_old_fields'),
+        ('rca', '0027_taxonomy_research_remove_old_fields'),
+        ('rca', '0027_taxonomy_news_item_remove_old_fields'),
+    ]
+
+    operations = [
+    ]
