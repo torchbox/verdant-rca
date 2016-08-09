df66f25bf099d4bad163e0769d02a868ae476f3e
Karl Hobley <karl@torchbox.com>
2016-08-09 12:08:53 +0100

    Merge migration

---
+++ django-verdant/rca/migrations/0037_merge.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rca', '0025_remove_school_programme_from_projects'),
+        ('rca', '0027_taxonomy_sustainrca_project_remove_old_fields'),
+        ('rca', '0027_taxonomy_research_innovation_page_remove_old_fields'),
+        ('rca', '0036_merge'),
+        ('rca', '0027_taxonomy_pathway_page_remove_old_fields'),
+    ]
+
+    operations = [
+    ]
