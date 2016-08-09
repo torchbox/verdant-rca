436614d97c765f0a5d2b1f9be81d33855ccd5ceb
Karl Hobley <karl@torchbox.com>
2016-08-09 11:50:03 +0100

    Merge migration

---
+++ django-verdant/rca/migrations/0036_merge.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rca', '0027_taxonomy_blog_remove_old_fields'),
+        ('rca', '0035_taxonomy_event_item_remove_old_fields'),
+        ('rca', '0027_taxonomy_job_page_remove_old_fields'),
+        ('rca', '0027_taxonomy_rca_now_remove_old_fields'),
+    ]
+
+    operations = [
+    ]
