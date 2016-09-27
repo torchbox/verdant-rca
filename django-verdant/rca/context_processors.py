f56b2b1d663956a8209268dae3ebc90acdc6c91f
Karl Hobley <karl@torchbox.com>
2016-09-27 11:55:24 +0100

    Removed global_years template variable
    Not used

--- django-verdant/rca/context_processors.py
+++ django-verdant/rca/context_processors.py
@@ -3,7 +3,7 @@ from django.db.models import Max
 
 from taxonomy.models import Programme, School, Area
 
-from rca.models import YEARS, EVENT_LOCATION_CHOICES, EVENT_AUDIENCE_CHOICES, RESEARCH_TYPES_CHOICES, WORK_THEME_CHOICES, WORK_TYPES_CHOICES, STAFF_TYPES_CHOICES, INNOVATIONRCA_PROJECT_TYPES_CHOICES, SUSTAINRCA_CATEGORY_CHOICES
+from rca.models import EVENT_LOCATION_CHOICES, EVENT_AUDIENCE_CHOICES, RESEARCH_TYPES_CHOICES, WORK_THEME_CHOICES, WORK_TYPES_CHOICES, STAFF_TYPES_CHOICES, INNOVATIONRCA_PROJECT_TYPES_CHOICES, SUSTAINRCA_CATEGORY_CHOICES
 from reachout_choices import REACHOUT_PROJECT_CHOICES, REACHOUT_PARTICIPANTS_CHOICES, REACHOUT_THEMES_CHOICES, REACHOUT_PARTNERSHIPS_CHOICES
 from datetime import date
 
@@ -14,14 +14,11 @@ def global_vars(request):
     all_programmes = Programme.objects.all()
     current_year = 2016  # FIXME
 
-    years_until_current_year = [y for y in YEARS if int(y) <= current_year]
-
     return {
         'global_all_schools': all_schools.values_list('slug', 'display_name'),
         'global_all_programmes': all_programmes.values_list('slug', 'display_name'),
         'global_locations': EVENT_LOCATION_CHOICES,
         'global_areas': all_areas.values_list('slug', 'display_name'),
-        'global_years': years_until_current_year,
         'global_audiences': EVENT_AUDIENCE_CHOICES,
         'global_research_types': RESEARCH_TYPES_CHOICES,
         'global_work_themes': WORK_THEME_CHOICES,
