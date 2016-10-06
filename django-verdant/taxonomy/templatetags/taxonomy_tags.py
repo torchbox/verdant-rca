f2a18851d39bb53629d66ee972e4e85122e03834
Karl Hobley <karl@torchbox.com>
2016-10-06 15:18:36 +0100

    [Sentry#2575] Fix crash when *_historical_display_name tags are called with None

--- django-verdant/taxonomy/templatetags/taxonomy_tags.py
+++ django-verdant/taxonomy/templatetags/taxonomy_tags.py
@@ -6,9 +6,15 @@ register = template.Library()
 
 @register.simple_tag
 def school_historical_display_name(school, year):
+    if school is None:
+        return ''
+
     return school.get_display_name_for_year(year)
 
 
 @register.simple_tag
 def programme_historical_display_name(programme, year):
+    if programme is None:
+        return ''
+
     return programme.get_display_name_for_year(year)
