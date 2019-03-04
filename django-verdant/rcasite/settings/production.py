dc020975e1480cfeb849f3c71b2ebc2993732c4c
Rich Brennan <rich.brennan@torchbox.com>
2019-03-04 13:32:26 +0000

    [1016] remove media url scheme include from settings

--- django-verdant/rcasite/settings/production.py
+++ django-verdant/rcasite/settings/production.py
@@ -109,10 +109,6 @@ if 'STATIC_DIR' in env:
 if 'MEDIA_URL' in env:
     MEDIA_URL = env['MEDIA_URL']
 
-    # HACK: MEDIA_URL should include scheme and domain name so that URLs generate for the video embed on StreamPage work
-    if MEDIA_URL.startswith('/'):
-        MEDIA_URL = 'https://www.rca.ac.uk' + MEDIA_URL
-
 if 'MEDIA_DIR' in env:
     MEDIA_ROOT = env['MEDIA_DIR']
 
