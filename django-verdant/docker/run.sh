8e74bc279659dc3aa8e3ecc55e3fca3c7245a130
Karl Hobley <karl@torchbox.com>
2019-12-12 11:56:46 +0000

    Compress static on build (not on app start)

--- django-verdant/docker/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-django-admin compress --force
-# QuotaGuard qgtunnel is used to connect to an external service, in this case
-# LDAP, via a Static IP through a tunnel opened on the serve
- /app/bin/qgtunnel gunicorn rcasite.wsgi:application
