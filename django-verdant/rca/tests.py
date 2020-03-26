e4c9d7a492d5ab733155d2a9ca445a12d1935f2f
kevinhowbrook <kbhowbrook@gmail.com>
2020-03-26 15:34:48 +0000

    Add some tests for the navigation api fetch

--- django-verdant/rca/tests.py
+++ django-verdant/rca/tests.py
@@ -1,16 +1,31 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+import datetime
+import json
+import logging
+import warnings
+import mock
 
-Replace this with more appropriate tests for your application.
-"""
-
+import requests
+from django.conf import settings
 from django.test import TestCase
+from requests.exceptions import Timeout
+from rca.navigation import Navigation
+from rca.models import HomePage
 
+class TestNavigationFetch(TestCase):
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    @mock.patch("rca.navigation.requests.get")
+    def test_data_if_timeout(self, mock_get):
+        """ If a timeout is caught the should be an empty list"""
+        mock_get.side_effect = Timeout
+        nav = Navigation().get_navigation_data()
+        self.assertEqual(nav, [])
+
+    @mock.patch("rca.navigation.requests.get")
+    def test_page_renders_with_timeout(self, mock_get):
+        """ If there is a timeout for the get request when the page loads,
+        ensure the page still renders with the empty data so it doesn't bork the page"""
+        home_page = HomePage.objects.first()
+        mock_get.side_effect = Timeout
+        response = self.client.get(home_page.path)
+        self.assertTemplateUsed(response, "home/home_page.html")
+        self.assertEqual(response.render().status_code, 200)
