6f94103ed64540dc23e8c2d69b6434800ae8de30
Tom Readings <tom.readings@torchbox.com>
2021-01-14 16:10:56 +0000

    Check image type on student profile form

--- django-verdant/student_profiles/forms.py
+++ django-verdant/student_profiles/forms.py
@@ -221,14 +221,27 @@ class ImageForm(forms.Form):
         if self.max_size and img.size > self.max_size:
             raise forms.ValidationError(u'Please keep file size under 10MB. Current file size {}'.format(filesizeformat(img.size)))
 
-        try:
-            dt = Image.open(img)
-            dt.load()
-            if dt.format.upper() not in ('PNG', 'JPEG', 'GIF', 'MPO'):
-                raise forms.ValidationError(u'Only images of types JPEG and GIF are allowed. Please make sure that you save the image file as the specified format, instead of simply changing the file extension.')
+        import magic
+        ALLOWED_FILE_TYPES = (
+            "PNG image data",
+            "GIF image data",
+            "JPEG image data",
+        )
 
+        try:
+            file_type = magic.from_file(img)
+            if file_type.startswith(ALLOWED_FILE_TYPES):
+                raise forms.ValidationError(u'Only images of types JPEG and GIF are allowed. Please make sure that you save the image file as the specified format, instead of simply changing the file extension.')
         except IOError:
             raise forms.ValidationError(u'Only images of types JPEG and GIF are allowed. Please make sure that you save the image file as the specified format, instead of simply changing the file extension.')
+
+        # try:
+        #     dt = Image.open(img)
+        #     dt.load()
+        #     if dt.format.upper() not in ('PNG', 'JPEG', 'GIF', 'MPO'):
+        #         raise forms.ValidationError(u'Only images of types JPEG and GIF are allowed. Please make sure that you save the image file as the specified format, instead of simply changing the file extension.')
+        # except IOError:
+        #     raise forms.ValidationError(u'Only images of types JPEG and GIF are allowed. Please make sure that you save the image file as the specified format, instead of simply changing the file extension.')
 
         if self.min_dim:
             minX, minY = self.min_dim
